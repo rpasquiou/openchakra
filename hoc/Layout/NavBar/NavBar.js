fcdbcaacedfeaf49dd88a26561262c099e308f73
Sébastien Auvray <sebastien.auvray@free.fr>
2020-03-10 14:15:33 +0000

    Phone : "Proposer mes services"

--- hoc/Layout/NavBar/NavBar.js
+++ hoc/Layout/NavBar/NavBar.js
@@ -180,7 +180,7 @@ class NavBar extends Component {
     const logged = this.state.logged;
     const user = this.state.user;
     const maboutique = <MenuItem onClick={this.handleMenuClose}><Typography><Link href={`/shop?id_alfred=${user ? user._id : ''}`}><a className={classes.navbarLinkMobile}>Ma boutique</a></Link></Typography></MenuItem>;
-    const becomealfred = <MenuItem onClick={this.handleMobileMenuClose}><Typography><Link href={'/creaShop/creaShop'}><a className={classes.navbarLinkMobile}>Proposer services</a></Link></Typography></MenuItem>;
+    const becomealfred = <MenuItem onClick={this.handleMobileMenuClose}><Typography><Link href={'/creaShop/creaShop'}><a className={classes.navbarLinkMobile}>Proposer mes services</a></Link></Typography></MenuItem>;
     const mobileavatar =
       <React.Fragment>
         <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit" className={classes.theavatarbutton}>
