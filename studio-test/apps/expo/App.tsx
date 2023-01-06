d20225e363f1590117c2e91b44cd21bbe259e816
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-01-06 17:59:55 +0100

    added fonts for Expo

--- studio-test/apps/expo/App.tsx
+++ studio-test/apps/expo/App.tsx
@@ -1,7 +1,21 @@
+import 'expo-dev-client'
+import React from 'react'
 import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
+import { useFonts } from 'expo-font'
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+    RadikalBold: require('../../packages/app/assets/fonts/Radikal-Bold.ttf'),
+    Tomarik: require('../../packages/app/assets/fonts/Tomarik-DisplayLineShadow.ttf'),
+  })
+
+  if (!loaded) {
+    return null
+  }
+
   return (
     <Provider>
       <NativeNavigation />
