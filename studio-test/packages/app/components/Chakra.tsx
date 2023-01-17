6dc3587169da041b961d9710cae84002add10256
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-01-17 16:06:22 +0100

    tried to implement input type date (compile error)

--- studio-test/packages/app/components/Chakra.tsx
+++ studio-test/packages/app/components/Chakra.tsx
@@ -1,9 +1,10 @@
-import {PropsWithChildren, useState} from 'react'
+import {PropsWithChildren, useState, useEffect} from 'react'
 import { styled } from '@tamagui/core'
 import {Stack, XStack, Input, Button, Text, InputProps, useTheme} from 'tamagui'
 import {TextInput as NativeInput} from 'react-native'
 import { Platform } from 'react-native'
-// import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useFocusEffect } from '@react-navigation/native'
 
 
 
@@ -45,6 +46,82 @@ export const Flex = Box
 interface extendedinput extends InputProps{
   inputMode?: string
 }
+
+/* type modedateinput = 'date' | 'time'
+
+const InputDateTime = () => {
+
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState<modedateinput>('date');
+  const [show, setShow] = useState(false);
+  const [platform, setPlatform] = useState('')
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+  
+  const onWebChange = (event) => {
+
+    console.log('Le changement, c maintenant', event)
+
+    // const currentDate = selectedDate;
+    // setShow(false);
+    // setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    if (platform === 'android') {
+      setShow(false);
+      // for iOS, add a button that closes the picker
+    }
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+    setShow(true)
+  };
+  
+  const showTimepicker = () => {
+    showMode('time');
+    setShow(true)
+  };
+
+  useEffect(() => {
+    if (!platform) {
+      setPlatform(Platform.OS)
+    }
+  }, [platform])
+
+  
+
+  return (
+    <Stack>
+    
+    <Button onPress={showDatepicker} >Show date picker!</Button>
+    <Button onPress={showTimepicker} >Show time picker!</Button>
+  
+    {date && <Text>selected: {date && date.toLocaleString()}</Text>}
+    
+    {show && platform && platform !== 'web' ? (
+    <DateTimePicker 
+      value={date}
+      mode={mode}
+      is24Hour={true}
+      onChange={onChange}
+    />) : null}
+    {show && platform && platform === 'web' ? (
+    <input
+      type={'date'}
+      value={date.toLocaleString()}
+      onChange={onWebChange}
+    />) : null}
+    </Stack>
+  )
+} */
+
 
 
 export const WappizyInput = (
