1ac98e0ed3b3c095a1f23278c934a22d33d872a4
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-01-18 15:16:06 +0100

    updated test components

--- studio-test/packages/app/features/testcomponents.tsx
+++ studio-test/packages/app/features/testcomponents.tsx
@@ -5,6 +5,7 @@ import {
   ScrollView,
   H1,
   WappizyInput as Input,
+  WappizySwitch as Switch,
   Flex,
   IconButton,
   Box,
@@ -14,11 +15,13 @@ import {
   Sheet,
   XStack,
   YStack,
-  Label} from '@my/ui'
-import {ChevronDown, ChevronUp} from '@tamagui/lucide-icons'
+  Label
+} from '@my/ui'
+import {ChevronDown, ChevronUp, Plus} from '@tamagui/lucide-icons'
+import Date from 'app/components/dependencies/custom-components/Date'
 import {View} from 'react-native'
 import {TextLink, useLink} from 'solito/link'
-import {Plus} from '@tamagui/lucide-icons'
+import { AddIcon, InfoIcon } from '@chakra-ui/icons'
 
 
 /* import {
@@ -34,7 +37,8 @@ const Testcomponents = () => {
   const reload = false
 
   return (
-    <View style={{flex: 1}}>
+    // <View style={{flex: 1}}>
+    <ScrollView style={{flex: 1}}>
       
       <Flex flexDirection="column">
         <Label htmlFor='comp-LCRUV4GT9NNRM'>Label for input type=text</Label>
@@ -129,7 +133,7 @@ const Testcomponents = () => {
           id="comp-LCRV4ZBQ3PC20"
           data-reload={reload}
           backgroundColor="pink.500"
-          boxShadow={'15px 10px 10px #E6E6E6'}
+          // boxShadow={'15px 10px 10px #E6E6E6'}
         >
           <Text
             id="comp-LCRV4ZBQDP16G"
@@ -154,7 +158,8 @@ const Testcomponents = () => {
           id="comp-LCRV6OWGZB6O9"
           data-reload={reload}
           aria-label="icon"
-          icon={<Plus />}
+          hoverStyle={{}}
+          icon={<Plus color={'green'} />}
           // size="lg"
           // isRound
         />
@@ -162,11 +167,109 @@ const Testcomponents = () => {
           id="comp-LCRV7AQZQO3TP"
           data-reload={reload}
         >
-          Ceci est icon "info"
+          Icon de Chakra difficiles ou longues à intégrer. Voir du côté de Lucide-icons
         </Text>
       </Flex>
+      <Flex
+        id="comp-LCRV9PJIBYMDF"
+        data-reload={reload}
+        // backgroundImage="https://www.alfredplace.io/wp-content/uploads/2022/12/Illu-rdv-03.svg" // SVG not supported at the moment
+        backgroundImage="https://www.francetvinfo.fr/pictures/_fi8l5D12T7DHF2cUROY575X5TI/0x106:1024x681/944x531/filters:format(webp)/2023/01/17/63c7273a4faed_000-327g6kn.jpg"
+        backgroundSize="cover"
+        height={600}
+      >
+        <Text
+          id="comp-LCRV9PJISIT8L"
+          data-reload={reload}
+          // backgroundColor="blackAlpha.500"
+        >
+          Ceci est une flex avec un background image et un backgroundSize en
+          cover
+        </Text>
+      </Flex>
+      <Flex
+        id="comp-LCRVBVK9KQT0R"
+        data-reload={reload}
+      >
+        <Button
+          id="comp-LCRVC1INE9KE4"
+          data-reload={reload}
+          // variant="solid"
+          // size="md"
+          borderRadius={25}
+        >
+          Ceci est un button avec border radius de 26px
+        </Button>
+      </Flex>
+      <Flex
+        id="comp-LCRVDM3QBDQRB"
+        data-reload={reload}
+      >
+        {/* <InfoIcon
+          id="comp-LCRVDRDZMTVQC"
+          data-reload={reload}
+        /> */}
+        <Text
+          id="comp-LCRVFC94FAHCW"
+          data-reload={reload}
+        >
+          Ceci est un icon "info"
+        </Text>
+      </Flex>
+      <Flex
+        id="comp-LCRVES0KWV2C2"
+        data-reload={reload}
+      >
+        <Date
+          id="comp-LCRVES0K7KOK0"
+          color="green"
+          data-reload={reload}
+        />
+        <Date
+          id="comp-LCRVGZ7BW9OTJ"
+          data-reload={reload}
+          data-format={{ year: '2-digit', month: 'short', day: 'numeric' }}
+        />
+      </Flex>
+      <Flex
+        id="comp-LCRVEU67TJVUV"
+        data-reload={reload}
+      >
+        <Switch
+          size="$3"
+          id="comp-LCRVEU6818JH2"
+          data-reload={reload}
+        />
+        <Text
+          id="comp-LCRVFY6IV9BLG"
+          data-reload={reload}
+        >
+          Ceci est un bouton switch
+        </Text>
+      </Flex>
+      <Flex
+        id="comp-LCRVIIFR3IOEH"
+        data-reload={reload}
+      >
+        <Text
+          id="comp-LCRVIIFR0HCCL"
+          data-reload={reload}
+        >
+          Ceci est un bouton avec text et icon{' '}
+        </Text>
+        <Button
+          id="comp-LCRVIXEMA5B0S"
+          data-reload={reload}
+          // variant="solid"
+          // size="md"
+          // leftIcon={<ArrowBackIcon />}
+          // rightIcon={<ArrowBackIcon />}
+        >
+          Button text
+        </Button>
+      </Flex>
 
-    </View>)
+    </ScrollView>)
 }
 
 export {Testcomponents}
