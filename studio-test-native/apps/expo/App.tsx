3dd84ce02ee968b9d0b12c709b7cec1664664342
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-01-31 15:09:35 +0100

    Native webview

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -1,24 +1,40 @@
-import 'expo-dev-client'
-import React from 'react'
-import { NativeNavigation } from 'app/navigation/native'
-import { Provider } from 'app/provider'
-import { useFonts } from 'expo-font'
+import {WebView} from 'react-native-webview';
+import React, { useRef, useEffect } from 'react'
+import {
+    SafeAreaView,
+    StyleSheet,
+} from 'react-native';
+//import SplashScreen from 'react-native-splash-screen';
 
-export default function App() {
-  const [loaded] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
-    Radikal: require('app/assets/fonts/Radikal-Bold.ttf'),
-    Tomarik: require('app/assets/fonts/Tomarik-DisplayLineShadow.ttf'),
-  })
+const App = () => {
 
-  if (!loaded) {
-    return null
-  }
+  /**
+  useEffect(()=>{
+      SplashScreen.hide();
+  });
+  */
+  
+  const webviewRef = useRef(null);
 
   return (
-    <Provider>
-      <NativeNavigation />
-    </Provider>
+    <>
+      <SafeAreaView style={styles.flexContainer}  >
+        <WebView
+          startInLoadingState={true}
+          allowsBackForwardNavigationGestures
+          mediaPlaybackRequiresUserAction={true}
+          source={{ uri: "https://dekuple.my-alfred.io/" }}
+          ref={webviewRef}
+        />
+        </SafeAreaView>
+    </>
   )
-}
+};
+
+const styles = StyleSheet.create({
+    flexContainer: {
+        flex: 1
+    },
+});
+
+export default App
