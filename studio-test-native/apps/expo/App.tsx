4dbac48aab3d1cd4b7ac0509d30563faf38d7689
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2023-02-28 19:54:43 +0100

    Revert "update webview type (useWebkit supported)"
    This reverts commit 512e2d03b4113e46bcf03fc1a9492b6645889ab1.

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -1,37 +1,36 @@
-import {WebView} from 'react-native-webview'
-import {useRef, useState} from 'react'
+import {WebView} from 'react-native-webview';
+import { useRef, useState} from 'react'
 import {
-  SafeAreaView,
-  StyleSheet,
-} from 'react-native'
+    SafeAreaView,
+    StyleSheet,
+} from 'react-native';
 
 const App = () => {
 
   const [currentUrl, setCurrentUrl]=useState('')
 
-  const webviewRef = useRef(null)
+  const webviewRef = useRef(null);
 
   return (
     <>
-      <SafeAreaView style={styles.flexContainer} >
+      <SafeAreaView style={styles.flexContainer}  >
         <WebView
           startInLoadingState={true}
-          useWebKit={true}
           allowsBackForwardNavigationGestures
           mediaPlaybackRequiresUserAction={true}
-          source={{uri: 'https://fumoir.my-alfred.io'}}
+          source={{ uri: "https://fumoir.my-alfred.io" }}
           ref={webviewRef}
           onNavigationStateChange={({url}) => setCurrentUrl(url)}
         />
-      </SafeAreaView>
+        </SafeAreaView>
     </>
   )
-}
+};
 
 const styles = StyleSheet.create({
-  flexContainer: {
-    flex: 1,
-  },
-})
+    flexContainer: {
+        flex: 1
+    },
+});
 
 export default App
