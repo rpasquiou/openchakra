dccf584567376331ed947739a9256afd4e40290a
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2023-03-09 14:04:55 +0100

    removed SafeAreaView for Dekuple

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -1,17 +1,14 @@
-import {WebView} from 'react-native-webview';
-import React, { useRef, useState, useEffect } from 'react'
+import {WebView} from 'react-native-webview'
+import React, {useRef, useState, useEffect} from 'react'
 import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    Button,
-    TouchableHighlight,
-    NativeModules,
-    View
-} from 'react-native';
-//import SplashScreen from 'react-native-splash-screen';
+  Text,
+  TouchableHighlight,
+  NativeModules,
+  View,
+} from 'react-native'
+// import SplashScreen from 'react-native-splash-screen';
 import axios from 'axios'
-const {WithingsLink} = NativeModules;
+const {WithingsLink} = NativeModules
 
 const App = () => {
 
@@ -19,9 +16,9 @@ const App = () => {
   const [displaySetup, setDisplaySetup]=useState(false)
   const [currentUser, setCurrentUser]=useState(null)
 
-  const webviewRef = useRef(null);
+  const webviewRef = useRef(null)
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log(`WebView URL is ${currentUrl}`)
     setDisplaySetup(/setup-appareil/.test(currentUrl))
   }, [currentUrl])
@@ -31,13 +28,13 @@ const App = () => {
     WithingsLink.synchronizeDevice(mac_address, advertise_key)
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     axios.get('https://dekuple.my-alfred.io/myAlfred/api/studio/current-user')
       .then(({data}) => {
         const firstLogin=!currentUser
         setCurrentUser(data)
         axios.get(`https://dekuple.my-alfred.io/myAlfred/api/studio/user/${data._id}?fields=devices`)
-          .then(({data})=> {
+          .then(({data}) => {
             const device=data[0]?.devices[0]
             if (firstLogin && device) {
               startSync(device)
@@ -48,49 +45,41 @@ const App = () => {
         setCurrentUser(null)
       })
     setDisplaySetup(/setup-appareil/.test(currentUrl))
-  }, [currentUrl])
+  }, [currentUrl, currentUser])
 
   const accessToken=currentUser?.access_token
   const csrfToken=currentUser?.csrf_token
 
   return (
     <>
-      <SafeAreaView style={styles.flexContainer}  >
-        <WebView
-          startInLoadingState={true}
-          allowsBackForwardNavigationGestures
-          mediaPlaybackRequiresUserAction={true}
-          source={{ uri: "https://dekuple.my-alfred.io" }}
-          ref={webviewRef}
-          onNavigationStateChange={({url}) => setCurrentUrl(url)}
-        />
-        </SafeAreaView>
-        { displaySetup && currentUser &&
+      <WebView
+        startInLoadingState={true}
+        allowsBackForwardNavigationGestures
+        mediaPlaybackRequiresUserAction={true}
+        source={{uri: 'https://dekuple.my-alfred.io'}}
+        ref={webviewRef}
+        onNavigationStateChange={({url}) => setCurrentUrl(url)}
+      />
+      { displaySetup && currentUser &&
           <>
-          <View style={{alignItems:'center', backgroundColor: '#f5f6fa'}}>
-            <TouchableHighlight style={{margin: '2%', padding:'4%', backgroundColor:'#172D4D', borderRadius:30}} >
-            <Text
-              style={{color: '#ffffff'}}
-              onPress={()=>WithingsLink.openInstall(accessToken, csrfToken)}
-            >Ajouter un appareil</Text>
-            </TouchableHighlight>
-            <TouchableHighlight style={{margin: '2%', padding:'4%', backgroundColor:'#43ABB1', borderRadius:30}} >
-            <Text
-              style={{color: '#ffffff'}}
-              onPress={()=>WithingsLink.openSettings(accessToken, csrfToken)}
-            >Modifier un appareil</Text>
-            </TouchableHighlight>
-          </View>
+            <View style={{alignItems: 'center', backgroundColor: '#f5f6fa'}}>
+              <TouchableHighlight style={{margin: '2%', padding: '4%', backgroundColor: '#172D4D', borderRadius: 30}} >
+                <Text
+                  style={{color: '#ffffff'}}
+                  onPress={() => WithingsLink.openInstall(accessToken, csrfToken)}
+                >Ajouter un appareil</Text>
+              </TouchableHighlight>
+              <TouchableHighlight style={{margin: '2%', padding: '4%', backgroundColor: '#43ABB1', borderRadius: 30}} >
+                <Text
+                  style={{color: '#ffffff'}}
+                  onPress={() => WithingsLink.openSettings(accessToken, csrfToken)}
+                >Modifier un appareil</Text>
+              </TouchableHighlight>
+            </View>
           </>
-        }
+      }
     </>
   )
-};
-
-const styles = StyleSheet.create({
-    flexContainer: {
-        flex: 1
-    },
-});
+}
 
 export default App
