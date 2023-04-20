a0d3553493fc56823f8fefe2168fc8e1ff3673bc
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2023-04-20 18:06:43 +0200

    god save the login

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -1,5 +1,5 @@
 import {WebView} from 'react-native-webview'
-import {useEffect, useRef, useState} from 'react'
+import {useEffect, useState} from 'react'
 import axios from 'axios'
 import {
   NativeModules,
@@ -20,7 +20,7 @@ const App = () => {
           RNLinkModule.isUserHasSubscribed(data.id)
         })
         .catch(err => {
-          if (err.response?.status==401) { 
+          if (err.response?.status==401) {
           }
         })
     }
@@ -31,6 +31,10 @@ const App = () => {
       <WebView
         startInLoadingState={true}
         allowsBackForwardNavigationGestures
+        injectedJavaScript={saveLoginScript}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        onMessage={event => {}}
         mediaPlaybackRequiresUserAction={true}
         source={{uri: BASE_URL_TO_POINT}}
         onNavigationStateChange={({url}) => setCurrentUrl(url)}
@@ -38,5 +42,55 @@ const App = () => {
     </>
   )
 }
+
+
+const saveLoginScript = `
+  
+  const currentUrl = window.location.href
+  const origUrl = window.location.origin + "/"  // if login is on the root
+  const testPattern = /login|connexion/
+
+  const handleLoginParam = () => {
+
+    const localStorageId = 'loginitems'
+
+    const username = document.querySelector('input[type="email"]')
+    const password = document.querySelector('input[type="password"]')
+    password.setAttribute('autocomplete', 'current-password')
+
+    const isLocalStorage = localStorage.getItem(localStorageId);
+    
+    if (isLocalStorage) {
+      const data = JSON.parse(isLocalStorage)
+      username.value = data?.email
+
+      setTimeout(() => {
+        password.value = data?.password
+      }, 500)
+
+    }
+
+    const updateToLocalStorage = (e) => {
+      const item = {[e.currentTarget.type]: e.currentTarget.value}
+      var logItems = localStorage.getItem(localStorageId);
+
+      if (!logItems) {
+        localStorage.setItem(localStorageId, JSON.stringify(item))
+      } else {
+        const oldConfig = JSON.parse(logItems)
+        localStorage.setItem(localStorageId, JSON.stringify({...oldConfig, ...item}))
+      }
+    }
+
+    username.addEventListener('change', updateToLocalStorage)
+    password.addEventListener('change', updateToLocalStorage)
+  }
+  
+  if (testPattern.test(currentUrl) || currentUrl === origUrl) {
+    handleLoginParam()
+  } 
+
+
+`
 
 export default App
