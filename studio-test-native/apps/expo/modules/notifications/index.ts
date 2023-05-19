e8b29a1d2b8e227d995e12bf62ee6fa439f7e6b2
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-05-19 12:25:44 +0200

    marked notification code with catch

--- studio-test-native/apps/expo/modules/notifications/index.ts
+++ studio-test-native/apps/expo/modules/notifications/index.ts
@@ -9,29 +9,30 @@ export type Topics = Topic[] | []
 
 
 export async function handleSubscription({topicsToHandle, back = false}: {topicsToHandle: Topics, back: boolean}) {
-    const permissionenabled = await requestUserPermission()
-    if (permissionenabled) {
-      topicsToHandle.forEach(async (topic: Topic) => {
-        if (!back) {
-          await messaging()
-            .subscribeToTopic(topic?.name)
-            .then(() => console.log(`Subscribed to topic ${topic?.name}!`));
-          } else {
-
-            if (!topic?.permanent) {
-              await messaging()
-                .unsubscribeFromTopic(topic?.name)
-                .then(() => console.log(`Unsubscribed to topic ${topic?.name}!`));
-            }
+  const permissionenabled = await requestUserPermission()
+    .catch(e => console.error('permission enabled', e))
+  if (permissionenabled) {
+    topicsToHandle.forEach(async (topic: Topic) => {
+      if (!back) {
+        await messaging()
+          .subscribeToTopic(topic?.name)
+          .then(() => console.log(`Subscribed to topic ${topic?.name}!`))
+          .catch(e => console.error('subscribe to topic', e))
+        } else {
+          if (!topic?.permanent) {
+            await messaging()
+              .unsubscribeFromTopic(topic?.name)
+              .then(() => console.log(`Unsubscribed to topic ${topic?.name}!`))
+              .catch(e => console.error('Unsubscribe to topic', e))
           }
-      });
-    }
+        }
+    });
   }
-
-
+}
 
 async function requestUserPermission() {
-  const authStatus = await messaging().requestPermission();
+  const authStatus = await messaging().requestPermission()
+    .catch(e => console.error('request permission error', e))
   const enabled =
     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
