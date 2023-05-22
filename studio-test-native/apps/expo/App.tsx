55b347fc88f989c99949329ef760037babb2fcc6
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-05-22 17:10:06 +0200

    get to older version

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -2,85 +2,99 @@ import {WebView} from 'react-native-webview'
 import {useEffect, useState} from 'react'
 import KeyboardAvoidingView from './components/KeyboardAvoidingView'
 import axios from 'axios'
-import { handleSubscription, Topics } from './modules/notifications'
-import { TOPIC_PREFIX, SEPARATOR, ALL_SUFFIX } from './modules/notifications/config';
-
+import messaging from '@react-native-firebase/messaging';
 
 const BASE_URL_TO_POINT = 'https://fumoir.my-alfred.io'
 
+const TOPIC_PREFIX="user"
+const UNDERSCORE = "_"
+const ALL_SUFFIX ="ALL";
+
+const gentopics = ({userid}: {userid: string}):string[] => {
+  const currentSuffixTopics = [userid, ALL_SUFFIX]
+  return currentSuffixTopics.map(suffix => TOPIC_PREFIX + UNDERSCORE + suffix)
+}
 
 const App = () => {
 
   const [currentUrl, setCurrentUrl]=useState('')
-  const [currentUser, setCurrentUser]=useState(null)
-  const [topicsToHandle, setTopicsToHandle] = useState<Topics>([])
+  const [user, setUser]=useState(null)
+  const [registeredTopics, setRegisteredTopics] = useState([])
 
   useEffect(() => {
     axios.get(`${BASE_URL_TO_POINT}/myAlfred/api/studio/current-user`)
       .then(({data}) => {
-        setCurrentUser(data)
+        if (!user) { setUser(data) }
       })
       .catch(err => {
-        if (err.response?.status==401 && currentUser) { setCurrentUser(null) }
+        if (err.response?.status==401 && user) { setUser(null) }
       })
   }, [currentUrl])
 
 
   useEffect(() => {
-    if (topicsToHandle.length > 0) {
-      currentUser 
-        ? handleSubscription({topicsToHandle, back: false}) 
-        : handleSubscription({topicsToHandle, back: true})
+
+    async function wholeStory({back = false}) {
+      const permissionenabled = await requestUserPermission()
+      if (permissionenabled) {
+
+        registeredTopics.forEach(async topicName => {
+          if (!back) {
+            await messaging()
+              .subscribeToTopic(topicName)
+              .then(() => console.log(`Subscribed to topic ${topicName}!`));
+            } else {
+              await messaging()
+                .unsubscribeFromTopic(topicName)
+                .then(() => console.log(`Unsubscribed to topic ${topicName}!`));
+            }
+        });
+      }
     }
-  }, [currentUser, topicsToHandle])
+    
+    user ? wholeStory({back: false}) : wholeStory({back: true})
+
+  }, [user])
 
   // Handle topics 
   useEffect(() => {
-    if (currentUser && topicsToHandle.length === 0) {
-      const topicsToRegister = gentopics({userid: currentUser?._id})
-      setTopicsToHandle(topicsToRegister)
+    if (user) {
+      const topicsToRegister = gentopics({userid: user?._id})
+      setRegisteredTopics(topicsToRegister)
     }
-  }, [currentUser])
-  
+  }, [user])
+
 
   return (
     <>
       <KeyboardAvoidingView>
-        <WebView
-          startInLoadingState={true}
-          allowsBackForwardNavigationGestures
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          onMessage={event => {}}
-          mediaPlaybackRequiresUserAction={true}
-          source={{uri: BASE_URL_TO_POINT}}
-          onNavigationStateChange={({url}) => setCurrentUrl(url)}
-        />
+      <WebView
+        startInLoadingState={true}
+        allowsBackForwardNavigationGestures
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        onMessage={event => {}}
+        mediaPlaybackRequiresUserAction={true}
+        source={{uri: BASE_URL_TO_POINT}}
+        onNavigationStateChange={({url}) => setCurrentUrl(url)}
+      />
       </KeyboardAvoidingView>
     </>
   )
 }
 
-export const gentopics = ({userid}: {userid: string}): Topics => {
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  const currentSuffixTopics = [
-    {
-      name: userid, 
-      permanent: false 
-    }, 
-    {
-      name: ALL_SUFFIX, 
-      permanent: true
-    }
-  ]
-  
-  // @ts-ignore
-  return currentSuffixTopics.reduce((acc, topic) => [...acc, {
-      permanent: topic?.permanent,
-      name: TOPIC_PREFIX + SEPARATOR + topic?.name
-    }], [])
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+
+  return enabled
 }
-
 
 
 export default App
