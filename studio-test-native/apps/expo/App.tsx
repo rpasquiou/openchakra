f94ba15ba4c74f973445faffd119e97b8ef940ac
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2023-06-07 13:16:35 +0200

    preparing new apps

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -1,11 +1,11 @@
 import {WebView} from 'react-native-webview'
 import {useEffect, useState} from 'react'
-import KeyboardAvoidingView from './components/KeyboardAvoidingView'
 import axios from 'axios'
+import KeyboardAvoidingView from './components/KeyboardAvoidingView'
 import NotifContainer from './modules/notifications/NotifContainer'
 
 
-const BASE_URL_TO_POINT = 'https://fumoir.my-alfred.io'
+const BASE_URL_TO_POINT = 'https://smartdiet.app'
 
 
 const App = () => {
@@ -13,9 +13,9 @@ const App = () => {
   const [currentUrl, setCurrentUrl]=useState('')
   const [currentUser, setCurrentUser]=useState(null)
 
-  const getCurrentUser = async () => {
+  const getCurrentUser = async() => {
     await axios.get(`${BASE_URL_TO_POINT}/myAlfred/api/studio/current-user`)
-      .then(res => setCurrentUser(res?.data) )
+      .then(res => setCurrentUser(res?.data))
       .catch(err => {
         if (err.response?.status==401 && currentUser) { setCurrentUser(null) }
       })
@@ -23,30 +23,29 @@ const App = () => {
 
   useEffect(() => {
     getCurrentUser()
-  }, [currentUrl])
+  }, [currentUrl, getCurrentUser])
   
 
   return (
     <>
       <KeyboardAvoidingView>
         <NotifContainer user={currentUser} allOnStart>
-        <WebView
-          startInLoadingState={true}
-          allowsBackForwardNavigationGestures
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          onMessage={event => {}}
-          mediaPlaybackRequiresUserAction={true}
-          source={{uri: BASE_URL_TO_POINT}}
-          sharedCookiesEnabled={true}
-          onNavigationStateChange={({url}) => setCurrentUrl(url)}
-        />
+          <WebView
+            startInLoadingState={true}
+            allowsBackForwardNavigationGestures
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            onMessage={event => {}}
+            mediaPlaybackRequiresUserAction={true}
+            source={{uri: BASE_URL_TO_POINT}}
+            sharedCookiesEnabled={true}
+            onNavigationStateChange={({url}) => setCurrentUrl(url)}
+          />
         </NotifContainer>
       </KeyboardAvoidingView>
     </>
   )
 }
-
 
 
 export default App
