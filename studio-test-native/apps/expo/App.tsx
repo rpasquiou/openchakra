47d26f7a582f139162497f2f0a4bffb9393162cc
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2023-06-22 13:09:31 +0200

    adaptation on App TIPI

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -1,18 +1,17 @@
 import React, {useCallback, useEffect, useState} from 'react'
-import { SafeAreaProvider, SafeAreaView, initialWindowMetrics } from 'react-native-safe-area-context';
 import {WebView} from 'react-native-webview'
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar'
 import axios from 'axios'
 import KeyboardAvoidingView from './components/KeyboardAvoidingView'
 import NotifContainer from './modules/notifications/NotifContainer'
 
 
-const BASE_URL_TO_POINT = 'https://smartdiet.app'
+const BASE_URL_TO_POINT = 'https://hellotipi.fr'
 
 const App = () => {
 
   const [currentUrl, setCurrentUrl]=useState('')
-  const [currentUser, setCurrentUser]=useState(null)  
+  const [currentUser, setCurrentUser]=useState(null)
 
   const getCurrentUser = useCallback(async() => {
     await axios.get(`${BASE_URL_TO_POINT}/myAlfred/api/studio/current-user`)
@@ -24,36 +23,31 @@ const App = () => {
 
   useEffect(() => {
     getCurrentUser()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUrl])
   
 
   return (
-    <SafeAreaProvider initialMetrics={initialWindowMetrics}>   
+    <>
       <NotifContainer user={currentUser} allOnStart>
-        <StatusBar style={"dark"} />
-        <SafeAreaView 
-          style={{flex: 1,}} 
-          edges={['top', 'left', 'right']}
-        >
-          <KeyboardAvoidingView>
-            <WebView
-              startInLoadingState={true}
-              allowsBackForwardNavigationGestures
-              javaScriptEnabled={true}
-              domStorageEnabled={true}
-              onMessage={event => {}}
-              mediaPlaybackRequiresUserAction={true}
-              source={{uri: BASE_URL_TO_POINT}}
-              sharedCookiesEnabled={true}
-              onNavigationStateChange={({url}) => setCurrentUrl(url)}
-            />
-          </KeyboardAvoidingView>
-        </SafeAreaView>
+        <StatusBar style={'dark'} />
+        <KeyboardAvoidingView>
+          <WebView
+            startInLoadingState={true}
+            allowsBackForwardNavigationGestures
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            onMessage={event => {}}
+            mediaPlaybackRequiresUserAction={true}
+            source={{uri: BASE_URL_TO_POINT}}
+            sharedCookiesEnabled={true}
+            onNavigationStateChange={({url}) => setCurrentUrl(url)}
+          />
+        </KeyboardAvoidingView>
       </NotifContainer>
-    </SafeAreaProvider>
+    </>
   )
 }
-
 
 
 export default App
