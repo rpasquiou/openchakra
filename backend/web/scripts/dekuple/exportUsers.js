ec2bd8533e0974de46f6b88bfe60342186fbebb3
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-08-24 12:09:15 +0200

    Dekuple: export users script

---
+++ backend/web/scripts/dekuple/exportUsers.js
@@ -0,0 +1,32 @@
+const { GENDER } = require('../../server/plugins/dekuple/consts')
+const moment = require('moment')
+require('moment/locale/fr')
+
+moment.locale('fr')
+const mongoose = require('mongoose')
+const { getDataModel, getDatabaseUri } = require('../../config/config')
+const { MONGOOSE_OPTIONS } = require('../../server/utils/database')
+const User=require('../../server/models/User')
+
+const formatDateTime = date => {
+  return moment(date).format('DD/MM/YYYY HH:mm')
+}
+
+const formatDate = date => {
+  return moment(date).format('DD/MM/YYYY')
+}
+
+mongoose.connect(getDatabaseUri(), MONGOOSE_OPTIONS)
+  .then(() => User.find())
+  .then(users => {
+    const res=users.map(m => {
+      return `${formatDate(m.creation_date)};${m.firstname};${m.lastname};${m.email};${m.phone};${GENDER[m.gender]};${formatDate(m.birthday)};${m.active}`
+    })
+    console.log(`Creation;firstname;lastname;email;phone;gender;birthday;active`)
+    console.log(res.join('\n'))
+    process.exit(0)
+  })
+  .catch(err => {
+     process.exit(1)
+     console.error(err)
+  })
