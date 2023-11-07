38740bd5f7a668e93dde180b8f36a053bafab283
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-11-07 23:32:58 +0100

    Klesia: add fonts

--- studio-test/src/dependencies/theme/Fonts.tsx
+++ studio-test/src/dependencies/theme/Fonts.tsx
@@ -30,7 +30,7 @@ const Fonts = () => (
       }
       @font-face {
         font-family: 'Futura';
-        src: local('Futura'), 
+        src: local('Futura'),
             url('fonts/Futura-medium.otf') format('opentype');
         font-weight: normal;
         font-style: normal;
@@ -44,35 +44,35 @@ const Fonts = () => (
       }
       @font-face {
         font-family: 'Cabin';
-        src: local('Cabin'), 
+        src: local('Cabin'),
             url('fonts/Cabin-Regular.ttf') format('truetype');
         font-weight: normal;
         font-style: normal;
       }
       @font-face {
         font-family: 'Cabin';
-        src: local('Cabin'), 
+        src: local('Cabin'),
             url('fonts/Cabin-Bold.ttf') format('truetype');
         font-weight: bold;
         font-style: normal;
       }
       @font-face {
         font-family: 'DancingScript';
-        src: local('DancingScript'), 
+        src: local('DancingScript'),
             url('fonts/DancingScript-Regular.ttf') format('truetype');
         font-weight: normal;
         font-style: normal;
       }
       @font-face {
         font-family: 'DancingScript';
-        src: local('DancingScript'), 
+        src: local('DancingScript'),
             url('fonts/DancingScript-Bold.ttf') format('truetype');
         font-weight: bold;
         font-style: normal;
       }
       @font-face {
         font-family: 'Playfair Display';
-        src: local('Playfair Display'), 
+        src: local('Playfair Display'),
             url('fonts/PlayfairDisplay-regular.ttf') format('truetype');
         font-weight: normal;
         font-style: normal;
@@ -81,30 +81,66 @@ const Fonts = () => (
       /* Fontes all-inclusive */
       @font-face {
         font-family: 'Crapaud';
-        src: local('Crapaud'), 
+        src: local('Crapaud'),
             url('fonts/crapaud_petit.ttf') format('truetype');
         font-weight: lighter;
         font-style: normal;
       }
       @font-face {
         font-family: 'Crapaud';
-        src: local('Crapaud'), 
+        src: local('Crapaud'),
             url('fonts/crapaud_gros.ttf') format('truetype');
         font-weight: bold;
         font-style: normal;
       }
       @font-face {
         font-family: 'berthold-akzidenz';
-        src: local('berthold-akzidenz-grotesk'), 
+        src: local('berthold-akzidenz-grotesk'),
             url('fonts/berthold-akzidenz-grotesk.otf') format('opentype');
         font-weight: normal;
         font-style: normal;
       }
       @font-face {
         font-family: 'berthold-akzidenz condensed';
-        src: local('berthold-akzidenz-grotesk condensed'), 
+        src: local('berthold-akzidenz-grotesk condensed'),
             url('fonts/berthold-akzidenz-grotesk-be-condensed.otf') format('opentype');
         font-weight: normal;
+        font-style: normal;
+      }
+      /* Fontes klesia */
+      @font-face {
+        font-family: 'Lato';
+        src: local('Lato'),
+            url('fonts/Lato-Regular.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+      }
+      @font-face {
+        font-family: 'Univers LT Std';
+        src: local('Univers LT Std'),
+            url('fonts/Univers LT Std 55 Roman.otf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+      }
+      @font-face {
+        font-family: 'Univers LT Std';
+        src: local('Univers LT Std'),
+            url('fonts/Univers LT Std 45 Light.otf') format('truetype');
+        font-weight: lighter;
+        font-style: normal;
+      }
+      @font-face {
+        font-family: 'Helvetica';
+        src: local('Helvetica'),
+            url('fonts/Helvetica.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+      }
+      @font-face {
+        font-family: 'Pilcrow rounded';
+        src: local('Pilcrow rounded'),
+            url('fonts/PilcrowRounded-Semibold.ttf') format('truetype');
+        font-weight: lighter;
         font-style: normal;
       }
       `}
@@ -113,6 +149,10 @@ const Fonts = () => (
 
 export const fontsName = [
   'Crapaud',
+  'Lato',
+  'Univers LT Std',
+  'Helvetica',
+  'Pilcrow rounded',
   'berthold-akzidenz',
   'berthold-akzidenz condensed'
 ]
