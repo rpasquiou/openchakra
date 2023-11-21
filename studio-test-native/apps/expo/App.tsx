53e921fcd534309e994ef9dee0948e443cf480dc
Sébastien Auvray <sebastien.auvray@wappizy.com>
2023-11-21 15:30:57 +0100

    mar. 21 nov. 2023 15:30:57 CET

--- studio-test-native/apps/expo/App.tsx
+++ studio-test-native/apps/expo/App.tsx
@@ -105,8 +105,7 @@ const App = () => {
               <TouchableHighlight style={{margin: '2%', padding: '4%', backgroundColor: '#172D4D', borderRadius: 30}} >
                 <Text
                   style={{color: '#ffffff'}}
-                  //onPress={() => WithingsLink.openInstall(accessToken, csrfToken)}
-                  onPress={() => alert('Ajouter')}
+                  onPress={() => WithingsLink.openInstall(accessToken, csrfToken)}
                 >Ajouter un appareil</Text>
               </TouchableHighlight>
               <TouchableHighlight style={{margin: '2%', padding: '4%', backgroundColor: '#43ABB1', borderRadius: 30}} >
