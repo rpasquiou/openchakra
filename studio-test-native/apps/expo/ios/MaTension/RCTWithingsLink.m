3ce29c2c2021da294e6af2d7988753ff88b2bda3
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-11-21 17:09:30 +0100

    Push

--- studio-test-native/apps/expo/ios/MaTension/RCTWithingsLink.m
+++ studio-test-native/apps/expo/ios/MaTension/RCTWithingsLink.m
@@ -15,7 +15,7 @@
 // accessToken: String, csrfToken: String
 RCT_EXPORT_METHOD(openInstall:(NSString *)accessToken csrfToken:(NSString *)csrfToken)
 {
- RCTLogInfo(@"openIstall %@,%@", accessToken, csrfToken);
+ RCTLogInfo(@"HEP HEP HEP openIstall %@,%@", accessToken, csrfToken);
 }
 
 RCT_EXPORT_METHOD(openSettings:(NSString *)accessToken csrfToken:(NSString *)csrfToken)
