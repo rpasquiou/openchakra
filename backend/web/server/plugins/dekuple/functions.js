2cbedf147d60517fa200bf35828f9917c9733b78
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-01-02 12:22:27 +0100

    Dekuple: fix polling measures

--- backend/web/server/plugins/dekuple/functions.js
+++ backend/web/server/plugins/dekuple/functions.js
@@ -287,7 +287,8 @@ isProduction() && cron.schedule('*/30 * * * * *', async () => {
   // Only get users having a device
   const fromDate=moment().add(-1, 'hour')
   return Device.find()
-    .then(devices => Promise.allSettled(devices.map(device => getNewMeasures({userid: device.user._id, startdate: fromDate})
+    .populate('user')
+    .then(devices => Promise.allSettled(devices.map(device => getNewMeasures({userid: device.user.withings_id, startdate: fromDate})
       .then(console.log)
       .catch(console.error)
     )
