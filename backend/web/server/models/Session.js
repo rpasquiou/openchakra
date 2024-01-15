a15c4c6e8306d49209a7994942cc371fa6b1498e
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-01-15 17:53:48 +0100

    Aftral: session

--- backend/web/server/models/Session.js
+++ backend/web/server/models/Session.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose')
-const {getDataModel} = require('../../config/config')
+const {getDataModel}=require('../../config/config')
 
-let SessionSchema=null
+let Session = null
 
 try {
-  SessionSchema=require(`../plugins/${getDataModel()}/schemas/SessionSchema`)
-  SessionSchema.plugin(require('mongoose-lean-virtuals'))
+  const Block = require(`./Block`)
+  if (Block) {
+    const SessionSchema=require(`../plugins/${getDataModel()}/schemas/SessionSchema`)
+    SessionSchema.plugin(require('mongoose-lean-virtuals'))
+    Session = Block.discriminator('session', SessionSchema)
+  }
 }
-catch(err) {
+catch (err) {
+  console.error(err)
   if (err.code !== 'MODULE_NOT_FOUND') {
     throw err
   }
 }
 
-module.exports = SessionSchema ? mongoose.model('session', SessionSchema) : null
+module.exports = Session
