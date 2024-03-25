a6bded44f1e04f8d0f532b0a8151ba40d0140252
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-03-25 14:53:47 +0100

    Sosynpl: missing enum declarations

--- backend/web/server/plugins/sosynpl/functions.js
+++ backend/web/server/plugins/sosynpl/functions.js
@@ -1,5 +1,5 @@
 const { declareVirtualField, declareEnumField } = require("../../utils/database");
-const { NATIONALITIES, WORK_MODE, SOURCE } = require("./consts");
+const { NATIONALITIES, WORK_MODE, SOURCE, EXPERIENCE } = require("./consts");
 
 const MODELS=['loggedUser', 'user', 'customer', 'freelance', 'admin']
 MODELS.forEach(model => {
@@ -34,7 +34,6 @@ declareVirtualField({model: 'freelance', field: 'freelance_missions', instance: 
     options: { ref: 'mission' }
   },
 })
-
 declareVirtualField({model: 'freelance', field: 'recommandations', instance: 'Array', multiple: true,
   caster: {
     instance: 'ObjectID',
@@ -50,4 +49,7 @@ declareVirtualField({model: 'freelance', field: 'communications', instance: 'Arr
 declareVirtualField({model: 'freelance', field: 'search_visible', instance: 'Boolean'})
 declareEnumField({model: 'freelance', field: 'work_mode', enumValues: WORK_MODE})
 declareEnumField({model: 'freelance', field: 'source', enumValues: SOURCE})
+declareEnumField({model: 'freelance', field: 'main_experience', enumValues: EXPERIENCE})
+declareEnumField({model: 'freelance', field: 'second_experience', enumValues: EXPERIENCE})
+declareEnumField({model: 'freelance', field: 'third_experience', enumValues: EXPERIENCE})
 
