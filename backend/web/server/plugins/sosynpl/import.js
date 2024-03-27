495f38373af2508ac1d549447d4af00bb5404c0b
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-03-27 15:11:06 +0100

    Sosybnpl: added sector model and import function

--- backend/web/server/plugins/sosynpl/import.js
+++ backend/web/server/plugins/sosynpl/import.js
@@ -2,7 +2,8 @@ const fs=require('fs')
 const lodash=require('lodash')
 const {importData, extractData}=require('../../../utils/import')
 const {XL_TYPE } = require('../../../utils/consts')
-const job=require('../../../server/models/Job')
+require('../../../server/models/Job')
+require('../../../server/models/Sector')
 const { normalize } = require('../../../utils/text')
 
 const loadRecords = async (path, tab_name, from_line) =>  {
@@ -22,6 +23,13 @@ const JOB_MAPPING={
 const JOB_KEY='name'
 const JOB_MIGRATION_KEY='name'
 
+const SECTOR_MAPPING={
+  name: `Secteurs`,
+}
+
+const SECTOR_KEY='name'
+const SECTOR_MIGRATION_KEY='name'
+
 const importJobs = async (input_file) => {
   let records=await loadRecords(input_file, `1 - Métiers`, 2)
   records=records.filter(r => !lodash.isEmpty(r['SO SYNPL à garder']))
@@ -30,7 +38,15 @@ const importJobs = async (input_file) => {
       migrationKey: JOB_MIGRATION_KEY})
 }
 
+const importSectors = async (input_file) => {
+  let records=await loadRecords(input_file, `Secteurs`)
+  records=lodash.orderBy(records, r => r[SECTOR_MAPPING.name])
+  console.log(records)
+  return importData({model: 'sector', data:records, mapping:SECTOR_MAPPING, identityKey: SECTOR_KEY, 
+      migrationKey: SECTOR_MIGRATION_KEY})
+}
+
 module.exports={
-  importJobs
+  importJobs, importSectors
 }
 
