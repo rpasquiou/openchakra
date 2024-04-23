fb10badf9411d89093bb5494997168540cb4b198
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-04-23 15:42:05 +0200

    Smartdiet: JIRA ticketing tests

--- backend/web/tests/smartdiet/tickets.test.js
+++ backend/web/tests/smartdiet/tickets.test.js
@@ -1,37 +1,46 @@
 const axios=require('axios')
 
-const POST_URL=`https://pro.smartdiet.fr/ws/application-ticket`
-
-axios.interceptors.request.use(function (config) {
-  // Log the POST data
-  console.log('POST Data:', config.headers);
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
-});
+jest.setTimeout(60000)
 
 describe('Jira tickets tests ', () => {
 
+  const DIET_EMAIL='stephanieb.smartdiet@gmail.com'
+
+  const CREATE_TICKET_URL=`https://pro.smartdiet.fr/ws/application-ticket`
+  const GET_URL=`https://pro.smartdiet.fr/ws/application-tickets-by-email/`
+  const CREATE_COMMENT_URL=`https://pro.smartdiet.fr/ws/application-ticket-comment`
+
   it('must create a ticket', async() => {
-    const config = {
-      headers: {
-        common: {
-          'Content-Type': 'text/plain'
-        }
-      }
-    };
-  
     const data={
       subject: 'Sujet du ticket',
       message: 'Message du ticket',
-      priority: 5,
-      sender: 'sebastien.auvray@wappizy.com',
-      tag: 'PATIENT'
+      priority: '5',
+      sender: DIET_EMAIL,
+      tag: 'diet'
     }
+    const res=await axios.post(CREATE_TICKET_URL, JSON.stringify(data))
+    expect(res.data).toEqual('OK')
+  })
 
-    const res=await axios.post(POST_URL, JSON.stringify(data), config).catch(console.error)
-    console.log(res)
+  it.only(`must get diet's tickets`, async () => {
+    const url=GET_URL+DIET_EMAIL
+    const res=await axios.get(url)
+    console.log(res.data)
+  })
+
+  it(`must add a comment on each diet's ticket`, async () => {
+    const url=GET_URL+DIET_EMAIL
+    const res=await axios.get(url)
+    const tickets=res.data
+    const promises=tickets.map(ticket => {
+      const data={
+        jiraid: ticket.jiraid.toString(),
+        message: `Commentaire sur le ticket jira #${ticket.jiraid}`
+      }
+      console.log(data)
+      return axios.post(CREATE_COMMENT_URL, JSON.stringify(data))
+    })
+    return Promise.all(promises)
   })
 
 })
