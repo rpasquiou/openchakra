713cf8b5faa41feab267af680f95721095d67483
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-04-23 18:19:59 +0200

    Smartdiet: ticketing comments WIP

--- backend/web/server/plugins/smartdiet/schemas/TicketCommentSchema.js
+++ backend/web/server/plugins/smartdiet/schemas/TicketCommentSchema.js
@@ -7,21 +7,14 @@ const Schema = mongoose.Schema
 
 const TicketCommentSchema = new Schema(
   {
-    ticket: {
-      type: Schema.Types.ObjectId,
-      ref: 'ticket',
-      required: [true, `Le ticket est obligatoire`],
+    jiraid: {
+      type: Number,
+      required: [true, `Le numéro de ticket est obligatoire`],
     },
-    text: {
+    message: {
       type: String,
-      required: [true, `Le texte est obligatoire`],
+      required: [true, `Le message est obligatoire`],
     },
-    /** Returned by Smartdiet */
-    date: {
-      type: Date,
-      required: false,
-    },
-    /** Returned by Smartdiet */ 
   }, schemaOptions,
 )
 
