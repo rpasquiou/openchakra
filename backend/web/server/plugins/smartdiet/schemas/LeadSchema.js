e3f25a945362b3da57431b9cda14ebb28d75fafd
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-04-23 12:19:18 +0200

    Smartdiet: fix leads company virtual

--- backend/web/server/plugins/smartdiet/schemas/LeadSchema.js
+++ backend/web/server/plugins/smartdiet/schemas/LeadSchema.js
@@ -3,7 +3,7 @@ const { isEmailOk } = require('../../../../utils/sms')
 const { isPhoneOk } = require('../../../../utils/sms')
 const mongoose = require('mongoose')
 const { schemaOptions } = require('../../../utils/schemas')
-const { CALL_STATUS, CALL_DIRECTION, COACHING_CONVERSION_STATUS } = require('../consts')
+const { CALL_STATUS, CALL_DIRECTION, COACHING_CONVERSION_STATUS, CALL_STATUS_TO_CALL } = require('../consts')
 const { DUMMY_REF } = require('../../../utils/database')
 
 const Schema = mongoose.Schema
@@ -55,7 +55,8 @@ const LeadSchema = new Schema({
   call_status: {
     type: String,
     enum: Object.keys(CALL_STATUS),
-    required: false,
+    default: CALL_STATUS_TO_CALL,
+    required: [true, `Le status d'appel est obligatoire`],
     set: v => v || undefined,
   },
   campain: {
