ebff8ae21d4b1d82f6e8ea0fd54fd94374eabad9
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-05-16 18:38:53 +0200

    SoSynpl: HSCategory.progress

--- backend/web/server/plugins/sosynpl/skills.js
+++ backend/web/server/plugins/sosynpl/skills.js
@@ -1,10 +1,48 @@
-const computeUserSkillsCategories = async (p1, p2, p3) => {
-  [p1, p2, p3].forEach((p, idx) => {
-    console.log('param', idx, p)
-  })
-  return [{name: 'cat1'}]
+const lodash=require('lodash')
+const HardSkillCategory = require("../../models/HardSkillCategory")
+const HardSkill = require("../../models/HardSkill")
+const { idEqual } = require("../../utils/database")
+const User = require('../../models/User')
+
+const computeUserHardSkillsCategories = async (userId, params, data) => {
+  const categories=await HardSkillCategory.find({parent: null})
+    .populate(['skills', {path: 'children', populate: ['children', 'skills']}])
+  const skills=[...(data.hard_skills_job || []), ...(data.hard_skills_extra || [])].map(s => s._id)
+  // Keep only categories containing user skills
+  const keep_category= (category) => {
+    // Has skill: keep only if contains user skills
+    if (category.skills.length>0) {
+      return category.skills.some(s => skills.some(us => idEqual(us, s._id)))
+    }
+    return category.children?.some(c => keep_category(c))
+  }
+  const map_category = category => {
+    return new HardSkillCategory({
+      ...category.toObject(),
+      children: category.children.filter(child => keep_category(child)).map(child => map_category(child)),
+      skills: category.skills.filter(s => skills.some(us => idEqual(us, s._id)))
+    })
+  }
+  const filtered_categories=categories
+    .filter(c => keep_category(c))
+    .map(c => map_category(c))
+  await Promise.all(filtered_categories.map(async c => c.progress=await computeHSCategoryProgress(userId, null, c)))
+  return filtered_categories
+}
+
+// COmpute for toplevel only
+const computeHSCategoryProgress = async (userId, params, data) => {
+  if (!!data.parent) {
+    return 0
+  }
+  const user=(await User.findById(userId))
+  const userSkills=[...(user.hard_skills_job||[]), ...(user.hard_skills_extra||[])]
+  const category=await HardSkillCategory.findById(data._id).populate({path: 'children', populate: 'skills' })
+  const categorySkills=lodash.flatten(category.children.map(child => child.skills), 2).map(s => s._id)
+  const commonSkills=lodash.intersectionBy(userSkills, categorySkills, id => id._id.toString())
+  return commonSkills.length/categorySkills.length
 }
 
 module.exports={
-  computeUserSkillsCategories,
+  computeUserHardSkillsCategories, computeHSCategoryProgress,
 }
