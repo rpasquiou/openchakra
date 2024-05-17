8fa7b2027a8b13799c96b7ef2bdb8d767486781e
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-05-17 11:50:53 +0200

    SoSynpl: compute User.available_hs_categories

--- backend/web/server/plugins/sosynpl/skills.js
+++ backend/web/server/plugins/sosynpl/skills.js
@@ -4,6 +4,7 @@ const HardSkill = require("../../models/HardSkill")
 const { idEqual } = require("../../utils/database")
 const User = require('../../models/User')
 
+// Returns HS categories tree for user job & extra skills
 const computeUserHardSkillsCategories = async (userId, params, data) => {
   const categories=await HardSkillCategory.find({parent: null})
     .populate(['skills', {path: 'children', populate: ['children', 'skills']}])
@@ -43,6 +44,33 @@ const computeHSCategoryProgress = async (userId, params, data) => {
   return commonSkills.length/categorySkills.length
 }
 
+// Returns HS categories tree for main_job only
+const computeUserHardSkillsJobCategories = async (userId, params, data) => {
+  const categories=await HardSkillCategory.find({parent: null})
+    .populate(['skills', {path: 'children', populate: ['children', 'skills']}])
+  const skills=data.main_job.job_file.hard_skills.map(s => s._id)
+  // Keep only categories containing user skills
+  const keep_category= (category) => {
+    // Has skill: keep only if contains user skills
+    if (category.skills.length>0) {
+      return category.skills.some(s => skills.some(us => idEqual(us, s._id)))
+    }
+    return category.children?.some(c => keep_category(c))
+  }
+  const map_category = category => {
+    return new HardSkillCategory({
+      ...category.toObject(),
+      children: category.children.filter(child => keep_category(child)).map(child => map_category(child)),
+      skills: category.skills.filter(s => skills.some(us => idEqual(us, s._id)))
+    })
+  }
+  const filtered_categories=categories
+    .filter(c => keep_category(c))
+    .map(c => map_category(c))
+  await Promise.all(filtered_categories.map(async c => c.progress=await computeHSCategoryProgress(userId, null, c)))
+  return filtered_categories
+}
+
 module.exports={
-  computeUserHardSkillsCategories, computeHSCategoryProgress,
+  computeUserHardSkillsCategories, computeHSCategoryProgress, computeUserHardSkillsJobCategories
 }
