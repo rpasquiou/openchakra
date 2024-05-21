8e07efc948c188b6cc180d8572728377421768bb
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-05-21 16:36:19 +0200

    SoSynpl: Freelance.softwares

---
+++ backend/web/server/plugins/sosynpl/schemas/ItemSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const bcrypt=require('bcryptjs')
+const {SPOON_SOURCE} = require('../consts')
+const {schemaOptions} = require('../../../utils/schemas')
+
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Le nom est obligatoire'],
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, 'Le compte est obligatoire'],
+  },
+}, schemaOptions)
+
+module.exports = ItemSchema
