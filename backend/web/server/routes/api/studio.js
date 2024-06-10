473129a1949a2ab24e42998c545ead47602f56e7
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-06-10 12:37:53 +0200

    BAck: trace GET start

--- backend/web/server/routes/api/studio.js
+++ backend/web/server/routes/api/studio.js
@@ -571,6 +571,7 @@ const loadFromRequest = (req, res) => {
   const user = req.user
 
   const logMsg=`GET ${model}/${id} ${fields} ...${JSON.stringify(params)}`
+  console.log(logMsg)
   console.time(logMsg)
 
   return loadFromDb({model, fields, id, user, params})
