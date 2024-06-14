5436e233abf0fef89b96c171c47041bf4b9557e8
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-06-14 15:35:27 +0200

    Aftral: search implementation

---
+++ backend/web/server/plugins/aftral-lms/schemas/SearchSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs')
+const { isEmailOk } = require("../../../../utils/sms")
+const { ROLES } = require("../consts")
+const { schemaOptions } = require("../../../utils/schemas")
+
+const Schema = mongoose.Schema
+
+const SearchSchema = new Schema({
+  pattern: {
+    type: String,
+    set: v => v?.trim(),
+    required: [true, `Le texte de recherche est obligatoire`],
+  },
+  blocks: [{
+    type: Schema.Types.ObjectId,
+    ref: 'block',
+  }],
+  users: [{
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  }],
+}, schemaOptions)
+
+/* eslint-disable prefer-arrow-callback */
+
+/* eslint-enable prefer-arrow-callback */
+
+module.exports = SearchSchema
---
+++ backend/web/server/plugins/aftral-lms/search.js
@@ -0,0 +1,64 @@
+const lodash=require('lodash')
+const { createRegExpOR, createRegExpAND } = require("../../../utils/text")
+const Block = require("../../models/Block")
+const User = require("../../models/User")
+
+const generateFilter = ({attributes, pattern, or}) => {
+  const regex=(or ? createRegExpOR:createRegExpAND)(pattern)
+  return {$or: attributes.map(att => ({[att]: regex}))}
+}
+const searchUsers = async (userId, params, data) => {
+  const limit=parseInt(params?.['limit.user']) || undefined
+  const page=parseInt(params?.['page.users']) || undefined
+  const orFilter = generateFilter({attributes: ['email', 'firstname', 'lastname'], pattern: data.pattern, or: true})
+  const andFilter = generateFilter({attributes: ['email', 'firstname', 'lastname'], pattern: data.pattern, or: false})
+  let query=User.find(andFilter)
+  if (page) {
+    query=query.skip(page*limit)
+  }
+  if (limit) {
+    query=query.limit(limit+1)
+  }
+  let res=await query
+  if (lodash.isEmpty(res)) {
+    query=User.find(orFilter)
+    if (page) {
+      query=query.skip(page*limit)
+    }
+    if (limit) {
+      query=query.limit(limit+1)
+    }
+    res=await query
+  }
+  return res
+}
+
+const searchBlocks = async (userId, params, data) => {
+  const limit=parseInt(params?.['limit.blocks']) || undefined
+  const page=parseInt(params?.['page.blocks']) || undefined
+  const orFilter = generateFilter({attributes: ['name', 'code'], pattern: data.pattern, or: true})
+  const andFilter = generateFilter({attributes: ['name', 'code'], pattern: data.pattern, or: false})
+  let query=Block.find(andFilter)
+  if (page) {
+    query=query.skip(page*limit)
+  }
+  if (limit) {
+    query=query.limit(limit+1)
+  }
+  let res=await query
+  if (lodash.isEmpty(res)) {
+    query=Block.find(orFilter)
+    if (page) {
+      query=query.skip(page*limit)
+    }
+    if (limit) {
+      query=query.limit(limit+1)
+    }
+    res=await query
+  }
+  return res
+}
+
+module.exports={
+  searchUsers, searchBlocks,
+}
