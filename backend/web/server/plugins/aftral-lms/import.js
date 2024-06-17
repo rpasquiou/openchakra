6f49ede79042ea5878a5679351d040272471b25c
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-06-17 15:38:56 +0200

    Aftral: resources import implementation

---
+++ backend/web/server/plugins/aftral-lms/import.js
@@ -0,0 +1,65 @@
+const fs=require('fs')
+const path=require('path')
+const file=require('file')
+const { splitRemaining } = require('../../../utils/text')
+const { importData } = require('../../../utils/import')
+const { RESOURCE_TYPE_EXCEL, RESOURCE_TYPE_PDF, RESOURCE_TYPE_PPT, RESOURCE_TYPE_VIDEO, RESOURCE_TYPE_WORD } = require('./consts')
+const { sendFileToAWS } = require('../../middlewares/aws')
+const User = require('../../models/User')
+require('../../models/Resource')
+
+const RESOURCE_MAPPING= userId => ({
+  name: `name`,
+  code: `code`,
+  url:  async ({record}) => (await sendFileToAWS(record.filepath, 'resource'))?.Location,
+  filepath: 'filepath',
+  resource_type: 'resource_type',
+  creator: () => userId,
+})
+
+const RESOURCE_KEY='code'
+
+const importResources = async (root_path, recursive) => {
+  const getResourceType = filepath => {
+    const extensionMapping={
+      xls: RESOURCE_TYPE_EXCEL,
+      xlsx: RESOURCE_TYPE_EXCEL,
+      pdf: RESOURCE_TYPE_PDF,
+      pps: RESOURCE_TYPE_PPT,
+      pptx: RESOURCE_TYPE_PPT,
+      ppsx: RESOURCE_TYPE_PPT,
+      mp4: RESOURCE_TYPE_VIDEO,
+      doc: RESOURCE_TYPE_WORD,
+      docx: RESOURCE_TYPE_WORD,
+    }
+    const ext=path.extname(filepath).split('.')[1]
+    const resource_type=extensionMapping[ext]
+    if (!resource_type && !!filepath) {
+      throw new Error(`${Object.keys(extensionMapping)} No type for ${ext} ${filepath}:${resource_type}`)
+    }
+    return resource_type
+  }
+  const splitCodeName = filepath => {
+    const basename=path.basename(filepath)
+    return splitRemaining(basename, ' ')
+  }
+  let filepaths=[]
+  const cb = async (directory,subdirectories, paths) => {
+    filepaths.push(...paths.map(p => [path.join(directory, p), ...splitCodeName(p), getResourceType(p)]))
+  }
+  const files=await file.walkSync(root_path, cb)
+  const records=filepaths.filter(t => !!t[2]).map(t => ({
+    filepath: t[0],
+    code: t[1],
+    name: t[2],
+    resource_type: t[3]
+  }))
+  const userId=(await User.findOne({email: 'hello+concepteur@wappizy.com'}))?._id
+  return importData({model: 'resource', data: records, mapping: RESOURCE_MAPPING(userId), identityKey: RESOURCE_KEY, migrationKey: RESOURCE_KEY})
+}
+
+
+module.exports={
+  importResources,
+}  
+
---
+++ backend/web/tests/aftral-lms/import.test.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const moment = require('moment')
+const path = require('path')
+const { MONGOOSE_OPTIONS } = require('../../server/utils/database')
+const { importResources } = require('../../server/plugins/aftral-lms/import')
+
+const ORIGINAL_DB=true
+const DBNAME=ORIGINAL_DB ? 'aftral-lms' : `test${moment().unix()}`
+const DROP=!ORIGINAL_DB
+
+const ROOT = path.join(__dirname, './data/Ressources')
+
+jest.setTimeout(120000)
+
+describe('Test imports', () => {
+
+  beforeAll(async () => {
+    await mongoose.connect(`mongodb://localhost/${DBNAME}`, MONGOOSE_OPTIONS)
+    console.log('Opened database', DBNAME)
+  })
+  
+  afterAll(async () => {
+    if (DROP) {
+      await mongoose.connection.dropDatabase()
+    }
+    await mongoose.connection.close()
+  })
+
+  it('must import resources', async () => {
+    return importResources(ROOT, true)
+  })
+
+
+})
+
