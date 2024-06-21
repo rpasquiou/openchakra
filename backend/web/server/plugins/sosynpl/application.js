27d282309faa9d4ab99726bd01e1fe0a136a7fdc
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-06-21 16:13:28 +0200

    Sosynpl: fix

--- backend/web/server/plugins/sosynpl/application.js
+++ backend/web/server/plugins/sosynpl/application.js
@@ -1,29 +1,32 @@
 const moment = require('moment')
 const { loadFromDb } = require('../../utils/database')
-const { BadRequestError } = require('../../utils/errors')
+const { BadRequestError, ForbiddenError } = require('../../utils/errors')
 const Mission = require('../../models/Mission')
 const Application = require('../../models/Application')
-const { APPLICATION_STATUS_REFUSED, REFUSE_REASON_CANCELED, REFUSE_REASON_PROVIDED, APPLICATION_STATUS_ACCEPTED } = require('./consts')
+const { APPLICATION_STATUS_REFUSED, REFUSE_REASON_PROVIDED, APPLICATION_STATUS_ACCEPTED } = require('./consts')
 
 const canAcceptApplication = async applicationId => {
-  const missionExists=await Mission.exists({application: applicationId})
+  const missionExists = await Mission.exists({ application: applicationId })
   if (missionExists) {
     throw new BadRequestError(`Une mission pour cette candidature existe déjà`)
   }
-  const application=await loadFromDb({model: 'application', id: applicationId, fields:['latest_quotations.expiration_date']})
+  const application = await loadFromDb({ model: 'application', id: applicationId, fields: ['latest_quotations.expiration_date'] })
   if (!(moment().isBefore(application[0]?.latest_quotations?.[0]?.expiration_date))) {
     throw new BadRequestError(`Le devis pour cette candidature est expiré`)
-  } 
+  }
+  if ([APPLICATION_STATUS_REFUSED, APPLICATION_STATUS_ACCEPTED].includes(application.status)) {
+    throw new ForbiddenError(`Cette candidature est déjà accepté ou annulée`)
+  }
 }
 
 const acceptApplication = async applicationId => {
-  const application=await Application.findById(applicationId).populate('announce').populate('latest_quotations')
+  const application = await Application.findById(applicationId).populate('announce').populate('latest_quotations')
   await Application.updateMany(
-    {announce: application.announce, _id: {$ne: application}}, 
-    {status: APPLICATION_STATUS_REFUSED, refuse_reason: REFUSE_REASON_PROVIDED, refuse_date: moment()}
+    { announce: application.announce, _id: { $ne: application } },
+    { status: APPLICATION_STATUS_REFUSED, refuse_reason: REFUSE_REASON_PROVIDED, refuse_date: moment() }
   )
-  application.status=APPLICATION_STATUS_ACCEPTED
-  application.accept_date=moment()
+  application.status = APPLICATION_STATUS_ACCEPTED
+  application.accept_date = moment()
   await application.save()
   console.log(application)
   return Mission.create({
@@ -37,6 +40,6 @@ const acceptApplication = async applicationId => {
 
 }
 
-module.exports={
+module.exports = {
   canAcceptApplication, acceptApplication,
 }
