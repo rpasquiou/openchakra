1a582be99d0d764ac08c199391e8a07eacc78b35
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-07-05 11:50:07 +0200

    Back: WIP fix csv loading

--- backend/web/tests/misc/excel.test.js
+++ backend/web/tests/misc/excel.test.js
@@ -4,11 +4,18 @@ const { extractData } = require('../../utils/import')
 const { TEXT_TYPE } = require('../../utils/consts')
 
 const XL_PATH=path.join(__dirname, '../data/test.csv')
+const XL_NEWLINES_PATH=path.join(__dirname, '../data/test_newlines.csv')
 
 describe('XL loading tests', () => {
 
   test('Must load csv', async() => {
     const contents=fs.readFileSync(XL_PATH)
+    const res=await extractData(contents, {format: TEXT_TYPE, delimiter: ';'})
+    console.log(res.records)
+  })
+
+  test.only('Must load csv with newlines', async() => {
+    const contents=fs.readFileSync(XL_NEWLINES_PATH)
     const res=await extractData(contents, {format: TEXT_TYPE, delimiter: ';'})
     console.log(res.records)
   })
