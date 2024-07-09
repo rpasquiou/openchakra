4d7d09321ecbad378aab063dab16e7801dcbc0b8
SeghirOumo <seghir.oumohand@wappizy.com>
2024-07-09 16:29:20 +0200

    Sosynpl[#55] Slider tooltip and min max, more to come

--- studio-test/src/dependencies/custom-components/Slider.tsx
+++ studio-test/src/dependencies/custom-components/Slider.tsx
@@ -3,12 +3,13 @@
   - Set border, borderRadius, m and p on ChakraSlider instead of SliderTrack. If done, fix overflow of SliderTrack and SliderThumb
 */
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import {
   Slider as ChakraSlider,
   SliderTrack,
   SliderFilledTrack,
-  SliderThumb
+  SliderThumb,
+  Tooltip
 } from "@chakra-ui/react"
 
 const Slider = React.forwardRef((props, ref) => {
@@ -21,14 +22,28 @@ const Slider = React.forwardRef((props, ref) => {
     p,
     m,
     borderRadius,
+    min = 0,
+    max = 100,
+    initialSliderValue = 50,
     ...rest
   } = props
+  console.log(props)
+  const [sliderValue, setSliderValue] = useState(initialSliderValue);
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  useEffect(() => {
+    setSliderValue(initialSliderValue);
+  }, [initialSliderValue]);
+  
   return (
     <ChakraSlider
       ref={ref}
       height={height}
       width={width}
       {...rest}
+      onChange={(v) => setSliderValue(v)}
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
     >
       <SliderTrack
         bg={backgroundColor}
@@ -38,15 +53,24 @@ const Slider = React.forwardRef((props, ref) => {
         width={width}
         p={p}
         m={m}
-        
       >
         <SliderFilledTrack bg={color} />
       </SliderTrack>
-      <SliderThumb
+      <Tooltip
+        hasArrow
         bg={color}
-        p={p}
-        m={m}
-      />
+        color='white'
+        placement='top'
+        isOpen={showTooltip}
+        border={"1px solid"}
+        label={`${sliderValue}%`}
+      >
+        <SliderThumb
+          bg={color}
+          p={p}
+          m={m}
+        />
+      </Tooltip>
     </ChakraSlider>
   )
 })
