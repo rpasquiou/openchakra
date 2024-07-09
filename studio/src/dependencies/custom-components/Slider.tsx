2ca642604bfc4c73fb3dddb468c8c4db4cd74a54
SeghirOumo <seghir.oumohand@wappizy.com>
2024-07-09 16:11:32 +0200

    Sosynpl[#56] added Chakra Slider and styling

--- studio/src/dependencies/custom-components/Slider.tsx
+++ studio/src/dependencies/custom-components/Slider.tsx
@@ -1,17 +1,54 @@
-import React, { useState, useEffect } from "react";
-import lodash from 'lodash'
-import { Box, Icon, Stack, Flex } from '@chakra-ui/react'
-import * as icons from 'lucide-react'
+/*TODO: 
+  - Use chakraStyles instead
+  - Set border, borderRadius, m and p on ChakraSlider instead of SliderTrack. If done, fix overflow of SliderTrack and SliderThumb
+*/
 
+import React from "react"
+import {
+  Slider as ChakraSlider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb
+} from "@chakra-ui/react"
 
 const Slider = React.forwardRef((props, ref) => {
-  const { backgroundColor, ...rest } = props
+  const {
+    backgroundColor,
+    color,
+    border,
+    height,
+    width,
+    p,
+    m,
+    borderRadius,
+    ...rest
+  } = props
   return (
-    <input type={'range'} {...props} orientation={'ver'} style={backgroundColor ? {backgroundColor}: {}}/>
+    <ChakraSlider
+      ref={ref}
+      height={height}
+      width={width}
+      {...rest}
+    >
+      <SliderTrack
+        bg={backgroundColor}
+        border={border}
+        borderRadius={borderRadius.base}
+        height={height}
+        width={width}
+        p={p}
+        m={m}
+        
+      >
+        <SliderFilledTrack bg={color} />
+      </SliderTrack>
+      <SliderThumb
+        bg={color}
+        p={p}
+        m={m}
+      />
+    </ChakraSlider>
   )
-  }
-);
+})
 
-Slider.displayName = "Slider";
-
-export default Slider;
+export default Slider
