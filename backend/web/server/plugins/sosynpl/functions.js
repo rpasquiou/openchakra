ab1fb74678c9c884612906b06d0dcb5670e53a58
SeghirOumo <seghir.oumohand@wappizy.com>
2024-07-18 14:20:34 +0200

    Sosynpl[#89]: fixed evaluation declaration in mission

--- backend/web/server/plugins/sosynpl/functions.js
+++ backend/web/server/plugins/sosynpl/functions.js
@@ -459,7 +459,12 @@ declareVirtualField({
 })
 
 //Mission
-declareVirtualField({model: 'mission', field: 'evaluation', instance: 'evaluation'})
+declareVirtualField({model: 'mission', field: 'evaluation', instance: 'evaluation',
+  caster: {
+    instance: 'ObjectID',
+    options: { ref: 'evaluation' }
+  },
+})
 
 const soSynplRegister = props => {
   console.log(`Register with ${JSON.stringify(props)}`)
