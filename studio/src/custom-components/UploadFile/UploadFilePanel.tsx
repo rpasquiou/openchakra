4930d13d58cd0eee83159cf1538a0b6c60350c99
SeghirOumo <seghir.oumohand@wappizy.com>
2024-08-02 12:28:56 +0200

    Sosynpl[#77]: added file name on upload file, still working on preview

--- studio/src/custom-components/UploadFile/UploadFilePanel.tsx
+++ studio/src/custom-components/UploadFile/UploadFilePanel.tsx
@@ -6,11 +6,19 @@ import TextControl from '~components/inspector/controls/TextControl'
 const UploadFilePanel = () => {
   
   const noticemsg = usePropsSelector('notifmsg')
+  const previewmsg = usePropsSelector('previewmsg')
+  const preview = usePropsSelector('preview')
 
   return (
     <>
       <SwitchControl label="Confirmation Message" name="notifmsg" />
       {noticemsg && <TextControl name="okmsg" label="OK message" />}
+
+      <SwitchControl label="Preview Message" name="previewmsg" />
+      {previewmsg && <TextControl name="prvmsg" label="Preview message" />}
+
+      <SwitchControl label="Preview" name="preview" />
+      {previewmsg && <TextControl name="prv" label="Preview" />}
     </>
   )
 }
