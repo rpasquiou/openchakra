48aa72c35fd9bb529a660e93b425d06e59e7abdd
Bastien Seree <bastien.seree@wappizy.com>
2024-08-26 10:54:47 +0200

    [#8] Fix the comment in preprocessGet

--- backend/web/server/plugins/cyberleague/functions.js
+++ backend/web/server/plugins/cyberleague/functions.js
@@ -121,7 +121,7 @@ const preprocessGet = async ({model, fields, id, user, params}) => {
     model='user'
     id = user?._id || 'INVALIDID'
   }
-  //if id it is a get for a group, else it is for homepage posts
+  //if id is defined it is a get for a group, else it is for homepage posts
   if (model==`post`&&!id) {
     params.filter = {group: null}
   }
