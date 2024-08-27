d31ccc144303d4110d1bb6579bd8e413b07776bd
Bastien Seree <bastien.seree@wappizy.com>
2024-08-27 17:46:00 +0200

    Db: migration script to change old admin attribute to administrator array

--- backend/web/server/plugins/cyberleague/database_update.js
+++ backend/web/server/plugins/cyberleague/database_update.js
@@ -1,6 +1,6 @@
-const lodash=require('lodash')
 const User = require('../../models/User')
 const { ROLE_ADMIN, ROLE_MEMBER, ROLE_PARTNER } = require('./consts')
+const Company = require('../../models/Company')
 
 const log = (...params) => {
   return console.log(`DB Update`, ...params)
@@ -23,9 +23,21 @@ const normalizeRoles = async () => {
   ))
 }
 
+const updateCompanyAdmin = async () => {
+  log(`Updating admin attribute`)
+
+  const updateAdmin = async (company) => {
+    const admin=company.toObject().admin
+    Company.collection.updateOne({_id: company._id}, {$set: {administrators: [admin]}, $unset: {admin: 1}})
+  }
+  return Company.find({admin:{$ne: undefined}})
+    .then(companies => Promise.all(companies.map(c => updateAdmin(c))))
+}
+
 const databaseUpdate = async () => {
   console.log('************ UPDATING DATABASE')
   await normalizeRoles()
+  await updateCompanyAdmin()
 }
 
 module.exports=databaseUpdate
