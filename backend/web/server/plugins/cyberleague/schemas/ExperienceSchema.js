7ab03c3ecaba4446408087e25d3dd0e07f7d43dd
Bastien Seree <bastien.seree@wappizy.com>
2024-08-27 12:18:36 +0200

    [#41] Add ExperienceSchema

---
+++ backend/web/server/plugins/cyberleague/schemas/ExperienceSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const {schemaOptions} = require('../../../utils/schemas')
+
+const Schema = mongoose.Schema
+
+const ExperienceSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, `L'utilisateur' est obligatoire`]
+  },
+  position: {
+    type: String,
+    required: [true, `La fonction est obligatoire`],
+  },
+  start_date: {
+    type: Date,
+    required: [true, `La date de début est obligatoire`],
+  },
+  end_date: {
+    type: Date,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: [true, `La description est obligatoire`],
+  },
+  company_name: {
+    type: String,
+    required: [true, `La compagnie est obligatoire`],
+  },
+}, {...schemaOptions})
+
+/* eslint-disable prefer-arrow-callback */
+/* eslint-enable prefer-arrow-callback */
+
+module.exports = ExperienceSchema
