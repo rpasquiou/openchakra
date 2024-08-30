5d24b65cb416c7ffe7ace7cb71eeb2aa59a55188
Bastien Seree <bastien.seree@wappizy.com>
2024-08-30 15:15:43 +0200

    [#78] Fix typo in ExpertiseSetSchema definition

--- backend/web/server/plugins/cyberleague/schemas/ExpertiseSetSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/ExpertiseSetSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
-const { idEqual } = require('../../../utils/database')
+const { idEqual, DUMMY_REF } = require('../../../utils/database')
 
 const Schema = mongoose.Schema
 
@@ -25,7 +25,7 @@ const ExpertiseSet = new Schema({
 
 /* eslint-disable prefer-arrow-callback */
 
-ExpertiseSet.virtual('dispay_expertises', DUMMY_REF).get(function() {
+ExpertiseSet.virtual('display_expertises', DUMMY_REF).get(function() {
   return this.categories.map(function (cat) {cat.find(function (ce) {this.expertises.some(e => idEqual(ce,e))})})
 })
 
