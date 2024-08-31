a7fdb2ab15b4480436daf32eac4886139526c2cd
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-08-31 23:36:32 +0200

    Aftral: speedup countChildren script

--- backend/web/scripts/aftral-lms/countChildren.js
+++ backend/web/scripts/aftral-lms/countChildren.js
@@ -9,14 +9,36 @@ if (!rootId) {
   console.error('expected rootId parameter')
 }
 
+const displayBlock = async blockId => {
+  const block=await Block.findById(blockId)
+  console.log(lodash.pick(block.toObject(), ['name', 'type']))
+}
+
 const countChildren = async blockId => {
-  const childrenIds=await Block.find({parent: blockId}, {_id:1})
-  console.log()
-  const subChildrenCount=await Promise.all(childrenIds.map(c => countChildren(c)))
-  return lodash.sum([childrenIds.length, ...subChildrenCount])
+  const result = await Block.aggregate([
+    {$match: { _id: mongoose.Types.ObjectId(blockId) }},
+    {
+        $graphLookup: {
+            from: "blocks",            
+            startWith: "$_id",         
+            connectFromField: "_id",   
+            connectToField: "parent",  
+            as: "descendants"          
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            descendantsCount: { $size: "$descendants" }  // Count the number of descendants
+        }
+    }
+  ])
+
+  return result.length > 0 ? result[0].descendantsCount : 0
 }
 
 mongoose.connect(`mongodb://localhost/aftral-lms`, MONGOOSE_OPTIONS)
+  .then(() => displayBlock(rootId))
   .then(() => countChildren(rootId))
   .then(count => console.log(`Block ${rootId} has ${count} children`))
   .catch(console.error)
