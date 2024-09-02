e5fbb4f9871919ca5f0f166655ff5b110caaf4d8
Batiste1998 <batiste.blactot@wappizy.com>
2024-09-02 11:13:27 +0200

    [#89] - add company id with a response type of array

--- backend/web/server/plugins/cyberleague/functions.js
+++ backend/web/server/plugins/cyberleague/functions.js
@@ -29,6 +29,13 @@ USER_MODELS.forEach(m => {
   declareVirtualField({ model: m, field: 'shortname', instance: 'String',requires:'firstname,lastname'})
   declareVirtualField({ model: m, field: 'pinned_by_count', instance: 'Number' })
   declareVirtualField({
+    model: m,
+    field: 'companies',
+    instance: 'Array',
+    multiple: true,
+    caster: { instance: 'ObjectID', options: { ref: 'company' } },
+  })
+  declareVirtualField({
     model: m, field: 'pinned_users', instance: 'Array', multiple: true,
     caster: {
       instance: 'ObjectID',
