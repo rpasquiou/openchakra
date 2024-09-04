47bef6e235c8995bc1f1dd68d6963ef73a309889
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-09-04 15:49:34 +0200

    Aftral: move tests baseData.js

---
+++ backend/web/tests/aftral-lms/baseData.js
@@ -0,0 +1,32 @@
+const { RESOURCE_TYPE_LINK, ACHIEVEMENT_RULE_CONSULT, ROLE_CONCEPTEUR } = require("../../../server/plugins/aftral-lms/consts")
+
+const BaseUser={
+  firstname: 'Firstname',
+  lastname: 'Lastname',
+  password: 'Password1;'
+}
+
+const BaseBuilder={
+  ...BaseUser,
+  role: ROLE_CONCEPTEUR,
+  email: 'hello+concepteur@wappizy.com',
+}
+
+const BaseResource={
+  name: 'Resource test',
+  resource_type: RESOURCE_TYPE_LINK,
+  achievement_rule: ACHIEVEMENT_RULE_CONSULT,
+  url: 'tagada',
+}
+
+const BaseModule={
+  name: 'Module test',
+}
+
+const BaseSequence={
+  name: 'Séquence test',
+}
+
+module.exports={
+  BaseResource, BaseBuilder, BaseModule, BaseSequence,
+}
