92fb9da4c7b14c1973c92cb2c3e295b20bfebeee
Bastien Seree <bastien.seree@wappizy.com>
2024-09-10 15:39:22 +0200

    Cyberleague: [#117] Refactor getRelated

--- backend/web/server/plugins/cyberleague/related.js
+++ backend/web/server/plugins/cyberleague/related.js
@@ -1,17 +1,16 @@
 const lodash = require('lodash')
 const { loadFromDb, idEqual } = require("../../utils/database")
 
+const compareCompanies = (company, expertises) => {
+  return lodash.intersectionBy([expertises, company.expertise_set?.expertises], 
+    (e1,e2) => idEqual(e1?._id, e2?._id)).length
+}
+
 const getRelated = (model) => {
   if (model == `company`) {
    return async (userId, params, data) => {
-    
-    const compareCompanies = (company) => {
-      return lodash.intersectionBy([data.expertise_set?.expertises, company.expertise_set?.expertises], 
-        (e1,e2) => idEqual(e1?._id, e2?._id)).length
-    }
-
-    const companies = await loadFromDb({model: `company`, fields: [`expertise_set.expertises`]})    
-    return lodash.orderBy(lodash.filter(companies, (c) => !idEqual(data._id,c._id)), (c) => compareCompanies(c), `desc`).slice(0, 10)
+    const companies = await loadFromDb({model: `company`, fields: [`expertise_set.expertises`]})
+    return lodash.orderBy(lodash.filter(companies, (c) => !idEqual(data._id,c._id)), (c) => compareCompanies(c, data.expertise_set?.expertises), `desc`).slice(0, 10)
    } 
   }
 }
