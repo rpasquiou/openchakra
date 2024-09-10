aa680f9663e8228b1469db16da98a520914ab371
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-09-10 10:23:28 +0200

    Smartdiet: generate password length 15

--- backend/web/utils/passwords.js
+++ backend/web/utils/passwords.js
@@ -1,17 +1,21 @@
 const crypto=require('crypto')
 const {getDataModel}=require('../config/config')
 const { BadRequestError } = require('../server/utils/errors')
+const RandExp=require('randexp')
 
 const DEFAULT_PASSWORD_PATTERN=/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
+const DEFAULT_PASSWORD_LENGTH=8
 const DEFAULT_PASSWORD_PATTERN_STR='8 caractères minimum dont une majuscule, une minuscule, un chiffre et un caractère spécial'
 
 let PASSWORD_PATTERN
+let PASSWORD_LENGTH
 let PASSWORD_PATTERN_STR
 
 // Get project custom password pattern if present
 try {
-  PASSWORD_PATTERN=require(`../server/plugins/${getDataModel()}/consts`).PASSWORD_PATTERN
-  PASSWORD_PATTERN_STR=require(`../server/plugins/${getDataModel()}/consts`).PASSWORD_PATTERN_STR
+  PASSWORD_PATTERN=require(`../server/plugins/${getDataModel()}/consts`).PASSWORD_PATTERN || DEFAULT_PASSWORD_PATTERN
+  PASSWORD_LENGTH=require(`../server/plugins/${getDataModel()}/consts`).PASSWORD_LENGTH || DEFAULT_PASSWORD_LENGTH
+  PASSWORD_PATTERN_STR=require(`../server/plugins/${getDataModel()}/consts`).PASSWORD_PATTERN_STR || DEFAULT_PASSWORD_PATTERN_STR
   if (!!PASSWORD_PATTERN != !!PASSWORD_PATTERN_STR) {
     console.error(`Password and password str are inconsistent:${PASSWORD_PATTERN} ${PASSWORD_PATTERN_STR}`)
     process.exit(0)
@@ -38,7 +42,13 @@ const validatePassword = async ({password, password2}) => {
 }
 
 const generatePassword = () => {
-  const length=8
+  const generator=new RandExp(new RegExp(`.{${PASSWORD_LENGTH}}`))
+  while (true) {
+    gen=generator.gen()
+    if (PASSWORD_PATTERN.test(gen)) {
+      return gen
+    }
+  }
   const wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
   return Array.from(crypto.randomFillSync(new Uint32Array(length)))
     .map((x) => wishlist[x % wishlist.length])
