e81eb445537234867f100c1f97bedeed5237fc7a
Bastien Seree <bastien.seree@wappizy.com>
2024-09-26 15:00:34 +0200

    Cyberleague: [#100] Add triple model and schema

---
+++ backend/web/server/plugins/cyberleague/schemas/TripleSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const { schemaOptions } = require('../utils/schemas')
+
+
+const Schema = mongoose.Schema
+
+const TripleSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Le nom est obligatoire'],
+  },
+  value: {
+    type: Number,
+    required: [true, 'La valeur est obligatoire'],
+  },
+  market_value: {
+    type: Number,
+    required: [true, 'La valeur du marché est obligatoire'],
+  }
+}, schemaOptions  )
+
+module.exports = TripleSchema
