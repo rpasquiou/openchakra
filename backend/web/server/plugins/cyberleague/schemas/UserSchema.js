f03fecff91db7ac7a864fed721f69909d49d0ccf
Bastien Seree <bastien.seree@wappizy.com>
2024-10-03 10:38:27 +0200

    Cyberleague: [#159] Remove company_size and company_sector fields in user schema

--- backend/web/server/plugins/cyberleague/schemas/UserSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/UserSchema.js
@@ -46,14 +46,6 @@ const UserSchema = new Schema({
     type: String,
     required: false,
   },
-  company_size: {
-    type: String,
-    required: false,
-  },
-  company_sector: {
-    type: String,
-    required: false,
-  },
   phone: {
     type: String,
     validate: [value => !value || isPhoneOk(value), 'Le numéro de téléphone doit commencer par 0 ou +33'],
