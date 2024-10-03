fcee4e05cd599bc78f43d9eed9c8f53f72eb96c5
Bastien Seree <bastien.seree@wappizy.com>
2024-10-03 17:17:36 +0200

    Cyberleague: [#192] migration script to match new company sizes

--- backend/web/server/plugins/cyberleague/database_update.js
+++ backend/web/server/plugins/cyberleague/database_update.js
@@ -1,5 +1,5 @@
 const User = require('../../models/User')
-const { ROLE_ADMIN, ROLE_MEMBER, ROLE_PARTNER, JOB_COMMERCIAL_MANAGER, JOB_GENERAL_MANAGER } = require('./consts')
+const { ROLE_ADMIN, ROLE_MEMBER, ROLE_PARTNER, JOB_COMMERCIAL_MANAGER, JOB_GENERAL_MANAGER, COMPANY_SIZE_11_50, COMPANY_SIZE_1001_PLUS } = require('./consts')
 const Company = require('../../models/Company')
 
 const log = (...params) => {
@@ -46,11 +46,27 @@ const normalizeJobs = async () => {
   ))
 }
 
+const normalizeCompanySize = async () => {
+  log(`Normalizing company sizes`)
+
+  const MAPPING={
+    COMPANY_SIZE_11_250: COMPANY_SIZE_11_50,
+    COMPANY_SIZE_5001_PLUS: COMPANY_SIZE_1001_PLUS
+  }
+
+  await Company.updateMany({size: null}, {size:COMPANY_SIZE_11_50})
+
+  return Promise.all(Object.entries(MAPPING).map(([oldSize, newSize]) => 
+    Company.updateMany({size:oldSize},{size: newSize})
+  ))
+}
+
 const databaseUpdate = async () => {
   console.log('************ UPDATING DATABASE')
   await normalizeRoles()
   await updateCompanyAdmin()
   await normalizeJobs()
+  await normalizeCompanySize()
 }
 
 module.exports=databaseUpdate
