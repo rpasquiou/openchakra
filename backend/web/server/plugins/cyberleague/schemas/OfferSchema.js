ab204434fb9bfb9a720c3be1b74bbc4e64d4445d
Bastien Seree <bastien.seree@wappizy.com>
2024-10-08 09:58:24 +0200

    Cyberleague: [#202] Add required attribute company to offer schema

--- backend/web/server/plugins/cyberleague/schemas/OfferSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/OfferSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const OfferSchema = new Schema({
-  
+  company: {
+    type: Schema.Types.ObjectId,
+    ref: 'company',
+    required: [true,`L'entreprise proposant l'offre est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
