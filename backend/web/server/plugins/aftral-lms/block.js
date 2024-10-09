a470aaf74e9455b9758fe167888885daa33925ec
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-10-09 11:30:06 +0200

    Aftral: fix getFinishedChildrenCount

--- backend/web/server/plugins/aftral-lms/block.js
+++ backend/web/server/plugins/aftral-lms/block.js
@@ -402,7 +402,7 @@ const getBlockFinishedChildren = async (userId, params, data, fields) => {
 const getFinishedChildrenCount = async (userId, params, data, fields) => {
   const children=await mongoose.models.block.find({parent: data._id, masked:{$ne: true}}, {_id: 1})
   const finished=await Progress.countDocuments({block: {$in: children}, user: userId, achievement_status: BLOCK_STATUS_FINISHED})
-  return finished.filter(Boolean).length
+  return finished
 }
 
 const getSessionConversations = async (userId, params, data, fields) => {
