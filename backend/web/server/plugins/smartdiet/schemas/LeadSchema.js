2568ec984153253f7ddf34320a161833b557aade
Batiste2003 <batiste.blactot1@gmail.com>
2024-10-10 10:35:42 +0200

    smartdiet - add formatted phone to Leads

--- backend/web/server/plugins/smartdiet/schemas/LeadSchema.js
+++ backend/web/server/plugins/smartdiet/schemas/LeadSchema.js
@@ -45,7 +45,11 @@ const LeadSchema = new Schema({
   phone: {
     type: String,
     validate: [value => !value || isPhoneOk(value), v => `Le numéro de téléphone '${v?.value}' doit commencer par 0 ou +33`],
-    set: v => v?.replace(/^0/, '+33'),
+    set: v => {
+      if (!v) return v;
+      const formatted = v.replace(/^0/, '+33').replace(/(\+33)(\d)(\d{2})(\d{2})(\d{2})(\d{2})/, '$1 $2 $3 $4 $5 $6');
+      return formatted;
+    },
     required: false,
   },
   // Calls attributes
