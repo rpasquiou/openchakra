f18dc8c46178bb2bcd767a2de271a566e1895ca3
Bastien Seree <bastien.seree@wappizy.com>
2024-10-10 16:26:25 +0200

    Cyberleague: fix company modification when it has multiple administrators

--- backend/web/server/plugins/cyberleague/functions.js
+++ backend/web/server/plugins/cyberleague/functions.js
@@ -11,6 +11,7 @@ const {
   setPostPutData,
   idEqual,
   loadFromDb,
+  setPrePutData,
 } = require('../../utils/database')
 const { ROLES, SECTOR, EXPERTISE_CATEGORIES, CONTENT_TYPE, JOBS, COMPANY_SIZE, ROLE_PARTNER, ROLE_ADMIN, ROLE_MEMBER, ESTIMATED_DURATION_UNITS, LOOKING_FOR_MISSION, CONTENT_VISIBILITY, EVENT_VISIBILITY, ANSWERS, QUESTION_CATEGORIES, SCORE_LEVELS, COIN_SOURCES, SCORE_LEVEL_1, SCORE_LEVEL_3, SCORE_LEVEL_2, STATUTS, GROUP_VISIBILITY, USER_LEVELS, CONTRACT_TYPES, WORK_DURATIONS, PAY, STATUT_SPONSOR, STATUT_FOUNDER, STATUSES, COMPLETION_FIELDS, STATUT_PARTNER, COMPLETED, OFFER_VISIBILITY, MISSION_VISIBILITY, COIN_SOURCE_LIKE_SHARE_COMMENT } = require('./consts')
 const { PURCHASE_STATUS, REGIONS } = require('../../../utils/consts')
@@ -600,6 +601,7 @@ const postCreate = async ({ model, params, data, user }) => {
 
 setPostCreateData(postCreate)
 
+
 const postPutData = async ({model, id, params, user}) => {
   if (model == `group`) {
     if (`users` in params) {
@@ -616,6 +618,19 @@ const postPutData = async ({model, id, params, user}) => {
 }
 
 setPostPutData(postPutData)
+
+
+const prePutData = async ({model, id, params, user}) => {
+
+  if (model == 'company') {
+    params.administrators = params.administrators.split(',')
+  }
+  
+  return {model, id, params, user}
+}
+
+setPrePutData(prePutData)
+
 
 module.exports = {
   ensureExpertiseCategories,
