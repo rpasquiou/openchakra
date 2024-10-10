e17ed2976dc59c51d39074905bc0a08fe75edd0a
Bastien Seree <bastien.seree@wappizy.com>
2024-10-10 10:53:01 +0200

    Cyberleague: [#226] add body required field to tip schema

--- backend/web/server/plugins/cyberleague/schemas/TipSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/TipSchema.js
@@ -4,6 +4,10 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const TipSchema = new Schema({
+  body: {
+    type: String,
+    required: [true, 'Le contenu est obligatoire'],
+  },
 }, schemaOptions)
 
 /* eslint-disable prefer-arrow-callback */
