32a54f91f99b9264cfb3067978ecc7197b9f60ab
Bastien Seree <bastien.seree@wappizy.com>
2024-10-15 16:10:43 +0200

    Cyberleague: [#159] Add fields to statist schema

--- backend/web/server/plugins/cyberleague/schemas/StatisticSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/StatisticSchema.js
@@ -1,11 +1,57 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
+const { ENOUGH_SCORES } = require('../consts')
 
 
 const Schema = mongoose.Schema
 
 const StatisticSchema = new Schema({
-
+  enoughScores: {
+    type: String,
+    enum: Object.keys(ENOUGH_SCORES)
+  },
+  securityIncidentManagement: {
+    type: Number
+  },
+  partner: {
+    type: Number
+  },
+  inventory: {
+    type: Number
+  },
+  insurance: {
+    type: Number
+  },
+  cyberRef: {
+    type: Number
+  },
+  intrusion: {
+    type: Number
+  },
+  externalized: {
+    type: Number
+  },
+  webApp: {
+    type: Number
+  },
+  antivirus: {
+    type: Number
+  },
+  charter: {
+    type: Number
+  },
+  financial: {
+    type: Number
+  },
+  sensibilization: {
+    type: Number
+  },
+  mfa: {
+    type: Number
+  },
+  admin: {
+    type: Number
+  },
 }, schemaOptions  )
 
 module.exports = StatisticSchema
