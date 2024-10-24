b01ec3dbf932d7912cdda8afd6d4ba8b82043ef3
Bastien Seree <bastien.seree@wappizy.com>
2024-10-24 09:53:14 +0200

    Cyberleague: [#248] Add creator required field in scan schema

--- backend/web/server/plugins/cyberleague/schemas/ScanSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/ScanSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const SchoolSchema = new Schema({
-  
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, `L créateur du score est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
