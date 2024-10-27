961f05e80d096fc29efc094200f59f4390859153
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-10-28 00:08:04 +0100

    Back: WIP Kmeet tests

--- backend/web/tests/misc/kmeet.test.js
+++ backend/web/tests/misc/kmeet.test.js
@@ -1,9 +1,8 @@
 const axios = require('axios')
-const request = require('request')
 
 describe('KMeet tests', () => {
 
-  const TOKEN=`DbwbZzYCp4mYZ0Wsfk8yj78PgkUBD-BegFBHfU2iowOwwSlt4hzJfGg4tc3w4Xs8pQdhOyZwk2oncToG`
+  const TOKEN=`y9UbZ_Gj1viBxbFQ5OG67hJTsf6lG1Ey6wOEW_q89tegqj1nKlDHBywcmJXbAGI3RnWYB7LL6O26hszw`
 
   test('Must create a room', async() => {
     const headers= {
@@ -12,33 +11,44 @@ describe('KMeet tests', () => {
     }
     const body={
       calendar_id: 1379426,
-      starting_at: "2024-11-01 09:00:00",
-      ending_at: "2024-11-01 11:00:00",
+      starting_at: "2024-10-25 19:30:00",
+      ending_at: "2024-11-25 22:00:00",
       timezone: "Europe\/Zurich",
       hostname: "kmeet.infomaniak.com",
       title: "example",
-      options: [{
+      options: {
         subject: "example",
         start_audio_muted: false,
-        enable_recording: true,
+        enable_recording: false,
         enable_moderator_video: false,
         start_audio_only: false,
         lobby_enabled: false,
-        password_enabled: true,
+        password_enabled: false,
         e2ee_enabled: true
-      }],
+      },
       attendees: [{
-        address: 'wilfrid.albersdorfer@wappizy.com',
+        address: 'sebastien.auvray@free.fr',
         state: 'NEEDS-ACTION',
     }],
     }
     // await axios.get('https://calendar.infomaniak.com/api/pim/calendar', {headers: headers})
-    //   .then(res => console.log(JSON.stringify(res.data.data.calendars[0], null,2)))
+    //   .then(res => console.log(JSON.stringify(res.data.data.calendars, null,2)))
     //   .catch(err => console.error(err.response.data))
     
-    await axios.post('https://api.infomaniak.com/1/kmeet/rooms', body, {headers: headers})
-      .then(console.log)
+    // await axios.post('https://api.infomaniak.com/1/kmeet/rooms', body, {headers: headers})
+    //   .then(console.log)
+    //   .catch(err => console.error(JSON.stringify(err.response.data, null, 2)))
+
+    const ROOM_ID='155910'
+
+    // await axios.get(`https://api.infomaniak.com/1/kmeet/rooms/${ROOM_ID}/settings`, {headers: headers})
+    //   .then(res => console.log(JSON.stringify(res.data, null,2)))
+    //   .catch(err => console.error(err.response.data))
+
+    await axios.delete(`https://api.infomaniak.com/1/kmeet/rooms/${ROOM_ID}`, {headers: headers})
+      .then(res => console.log(JSON.stringify(res.data, null,2)))
       .catch(err => console.error(err.response.data))
+
   })
 
 })
