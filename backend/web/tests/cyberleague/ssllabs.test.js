6780793e7d93101f8c719de663a073868385b81d
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-10-29 14:06:28 +0100

    Cyberleague: SSLLabs test

--- backend/web/tests/cyberleague/ssllabs.test.js
+++ backend/web/tests/cyberleague/ssllabs.test.js
@@ -10,9 +10,18 @@ describe(`score tests`, () => {
   afterAll(async () => {
   })
 
-  it(`must connect to SLL Labs`, async () => {
+  it(`must initiate SLL Labs scan`, async () => {
     const res=await axios.get(
-      'https://api.ssllabs.com/api/v4/analyze?host=smartdiet-validation.my-alfred.io&all=on', {
+      'https://api.ssllabs.com/api/v4/analyze?host=smartdiet-validation.my-alfred.io&all=on&s=13.36.62.148', {
+      headers: {email: EMAIL}
+      }
+    )
+    console.log(res.data)
+  })
+
+  it(`must get SLL Labs info`, async () => {
+    const res=await axios.get(
+      'https://api.ssllabs.com/api/v4/info', {
       headers: {email: EMAIL}
       }
     )
