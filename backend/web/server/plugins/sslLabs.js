8af6c2f08ecb676d0980a048e5e54241d6dc8942
Bastien Seree <bastien.seree@wappizy.com>
2024-10-31 12:04:16 +0100

    Cyberleague: [#270] commentary in ssllabs as a todo

--- backend/web/server/plugins/sslLabs.js
+++ backend/web/server/plugins/sslLabs.js
@@ -13,6 +13,8 @@ const sslScan = async (url, isNewScan) => {
     throw new Error(`L'url est invalide`)
   }
 
+  //check that there is not already a scan INPROGRESS for this url and this user
+
   //availability check
   const info=await axios.get(
     'https://api.ssllabs.com/api/v4/info', {
