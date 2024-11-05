7fc2234c87dcb23abbd99efdab4fa9d259cfc17c
Bastien Seree <bastien.seree@wappizy.com>
2024-11-05 11:09:51 +0100

    Back: [#258 Cyber] Add fucntion createNotificationSchema to use allowedTypes

--- backend/web/server/plugins/notifications/schemas/NotificationSchema.js
+++ backend/web/server/plugins/notifications/schemas/NotificationSchema.js
@@ -1,47 +1,55 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
 
-const Schema = mongoose.Schema
+const createNotificationSchema = (allowedTypes) => {
 
-const NotificationSchema = new Schema({
-  _target: {
-    type: Schema.Types.ObjectId,
-    refPath: '_target_type',
-    required: [true, `Il faut l'id de la cible de la notification`]
-  },
-  _target_type: {
-    type: String,
-    required: [true, `Le type de l'id de la target est obligatoire`]
-  },
-  _text: {
-    type: String,
-    required: [true, `Un texte est obligatoire pour une notification`]
-  },
-  recipients: {
-    type: [{
+  const Schema = mongoose.Schema
+
+  const NotificationSchema = new Schema({
+    _target: {
       type: Schema.Types.ObjectId,
-      ref: 'user',
-      required: true
-    }]
-  },
-  seen_by_recipients: {
-    type: [{
-      type: Schema.Types.ObjectId,
-      ref: 'user',
-      required: true
-    }]
-  },
-  picture: {
-    type: String,
-    required: false
-  },
-  custom_props: {
-    type: String,
-    required: false
-  },
-}, schemaOptions)
+      refPath: '_target_type',
+      required: [true, `Il faut l'id de la cible de la notification`]
+    },
+    _target_type: {
+      type: String,
+      required: [true, `Le type de l'id de la target est obligatoire`]
+    },
+    _text: {
+      type: String,
+      required: [true, `Un texte est obligatoire pour une notification`]
+    },
+    type: {
+      type: String,
+      required: [true, `Le type de la notification est obligatoire`],
+      enum: allowedTypes
+    },
+    recipients: {
+      type: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+      }]
+    },
+    seen_by_recipients: {
+      type: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+      }]
+    },
+    picture: {
+      type: String,
+      required: false
+    },
+    custom_props: {
+      type: String,
+      required: false
+    },
+  }, schemaOptions)
 
-/* eslint-disable prefer-arrow-callback */
-/* eslint-enable prefer-arrow-callback */
+  /* eslint-disable prefer-arrow-callback */
+  /* eslint-enable prefer-arrow-callback */
 
-module.exports = NotificationSchema
+  return NotificationSchema
+}
