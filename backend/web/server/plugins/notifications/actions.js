7aa6735a55596a571d7dcfd495b5deae94a21ad2
Bastien Seree <bastien.seree@wappizy.com>
2024-11-07 16:51:54 +0100

    Back: [#257cyber] add validate action definition in notifcation plugin

--- backend/web/server/plugins/notifications/actions.js
+++ backend/web/server/plugins/notifications/actions.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
 const { NotFoundError, ForbiddenError } = require("../../utils/errors");
+const { addAction } = require("../../utils/studio/actions");
+const { getModel } = require("../../utils/database");
+
+
+const validateNotification = async ({value}, user) => {
+  //check if value is a notif id in case validate action is used elsewhere
+  const model = await getModel(value)
+  if (model == 'notification') {
+    //add user to seen_by_recipients
+    await mongoose.models[model].findByIdAndUpdate(value,{$addToSet: {seen_by_recipients: user._id}})
+  }
+}
+
+addAction('validate',validateNotification)
+
+
 
 const isValidateNotificationAllowed = async ({dataId, user, ...rest}) => {
   const NotificationModel = mongoose.models.notification
