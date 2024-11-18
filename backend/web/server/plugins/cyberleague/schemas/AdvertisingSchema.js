846a161d8fa0dafcdf39b6b285efbd9c00aae883
Bastien Seree <bastien.seree@wappizy.com>
2024-11-18 15:42:22 +0100

    Cyberleague: [#306] add company required field to advertising schema

--- backend/web/server/plugins/cyberleague/schemas/AdvertisingSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/AdvertisingSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const AdvertisingSchema = new Schema({
-
+  company: {
+    type: Schema.Types.ObjectId,
+    ref: 'company',
+    required: [true, `L'entreprise créatrice de la publicité est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
