6b743cc89b05ec21954f55192a779cc1638e4dd3
Bastien Seree <bastien.seree@wappizy.com>
2024-11-21 11:28:24 +0100

    Cyberleague: [#328.1] add current_campaign field in admin dashboard schema

--- backend/web/server/plugins/cyberleague/schemas/AdminDashboardSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/AdminDashboardSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const AdminDashboardSchema = new Schema({
-
+  current_campaign: {
+    type: Schema.Types.ObjectId,
+    ref: 'company',
+    required: false
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
