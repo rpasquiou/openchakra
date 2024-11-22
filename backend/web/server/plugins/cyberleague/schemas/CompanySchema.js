40587eacae67b53477c4f59f20f9c476c6560b21
Bastien Seree <bastien.seree@wappizy.com>
2024-11-22 14:58:04 +0100

    Cyberleague: add default value for statut field in company schema

--- backend/web/server/plugins/cyberleague/schemas/CompanySchema.js
+++ backend/web/server/plugins/cyberleague/schemas/CompanySchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { schemaOptions } = require('../../../utils/schemas')
 const siret = require('siret')
 const { DUMMY_REF } = require('../../../utils/database')
-const { SECTOR, COMPANY_SIZE, LOOKING_FOR_MISSION, STATUTS, STATUT_FOUNDER, STATUT_SPONSOR, CURRENT_CAMPAIGN_STATUSES} = require('../consts')
+const { SECTOR, COMPANY_SIZE, LOOKING_FOR_MISSION, STATUTS, STATUT_FOUNDER, STATUT_SPONSOR, CURRENT_CAMPAIGN_STATUSES, STATUT_MEMBER} = require('../consts')
 const { isPhoneOk, isEmailOk } = require('../../../../utils/sms')
 const AddressSchema = require('../../../models/AddressSchema')
 
@@ -143,6 +143,7 @@ const CompanySchema = new Schema(
       enum: Object.keys(STATUTS),
       required: false,
       set: v => v || undefined,
+      default: STATUT_MEMBER
     },
     sponsor: {
       type: [{
