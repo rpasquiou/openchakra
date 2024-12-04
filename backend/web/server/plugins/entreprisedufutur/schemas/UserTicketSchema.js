59976923aff0fc85a7efa7c600f9d7f60712c68e
Bastien Seree <bastien.seree@wappizy.com>
2024-12-04 21:57:05 +0100

    EDF: [#36] add user field to user ticket

--- backend/web/server/plugins/entreprisedufutur/schemas/UserTicketSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/UserTicketSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const UserTicketSchema = new Schema({
-
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, `L'acheteur du ticket est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
