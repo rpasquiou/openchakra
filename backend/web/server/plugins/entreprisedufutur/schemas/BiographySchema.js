fe94226f11de60123dd027168ae3796d4c753299
Bastien Seree <bastien.seree@wappizy.com>
2024-12-04 22:07:01 +0100

    EDF: [#48] add user field to biography schema

--- backend/web/server/plugins/entreprisedufutur/schemas/BiographySchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/BiographySchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const BiographySchema = new Schema({
-
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, `La cible de la biographie est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
