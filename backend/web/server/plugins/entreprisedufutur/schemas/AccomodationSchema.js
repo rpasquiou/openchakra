ec181b7599c46a6116b3bbc05c7ad93d6eb92784
Bastien Seree <bastien.seree@wappizy.com>
2024-12-05 11:25:00 +0100

    EDF: [#41] add type field to accomodation schema

--- backend/web/server/plugins/entreprisedufutur/schemas/AccomodationSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/AccomodationSchema.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
+const { ACCOMODATION_TYPES } = require('../consts')
 
 const Schema = mongoose.Schema
 
 const AccomodationSchema = new Schema({
-
+  type: {
+    type: String,
+    enum: Object.keys(ACCOMODATION_TYPES),
+    required: [true, `Le type d'équipement est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
