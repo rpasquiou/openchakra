fef64ecb7090050369cc3d77bc29565b47479542
Bastien Seree <bastien.seree@wappizy.com>
2024-12-05 11:01:55 +0100

    EDF: [#29] add quantity field to eventTicket schema

--- backend/web/server/plugins/entreprisedufutur/schemas/EventTicketSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/EventTicketSchema.js
@@ -26,6 +26,10 @@ const EventTicketSchema = new Schema({
     type: String,
     required: false
   },
+  quantity: {
+    type: Number,
+    required: false
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
