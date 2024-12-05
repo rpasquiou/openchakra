a5c646232813ff1f69d14e6c94b6109894918b91
Bastien Seree <bastien.seree@wappizy.com>
2024-12-05 10:20:11 +0100

    EDF: [#56] add creator field to evaluation schema

--- backend/web/server/plugins/entreprisedufutur/schemas/EvaluationSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/EvaluationSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const EvaluationSchema = new Schema({
-
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, `Le créateur de l'avis est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
