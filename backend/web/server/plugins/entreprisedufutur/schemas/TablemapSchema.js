070ab7c8d18c3548533e820af1eceab4b86f81d8
Bastien Seree <bastien.seree@wappizy.com>
2024-12-06 10:45:41 +0100

    EDF: [#63] add event field to tablemap schema

--- backend/web/server/plugins/entreprisedufutur/schemas/TablemapSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/TablemapSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const TablemapSchema = new Schema({
-  
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: 'event',
+    required: [true, `L'événement du plan de table est obligatoire`]
+  }
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
