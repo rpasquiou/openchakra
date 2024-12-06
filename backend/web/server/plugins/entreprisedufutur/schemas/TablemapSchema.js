a7fd2ef7e7380779d321840f93f824d9cfe79978
Bastien Seree <bastien.seree@wappizy.com>
2024-12-06 10:44:05 +0100

    EDF: [#62] add tablemap schema

---
+++ backend/web/server/plugins/entreprisedufutur/schemas/TablemapSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const {schemaOptions} = require('../../../utils/schemas')
+
+const Schema = mongoose.Schema
+
+const TablemapSchema = new Schema({
+  
+}, {...schemaOptions})
+
+/* eslint-disable prefer-arrow-callback */
+/* eslint-enable prefer-arrow-callback */
+
+module.exports= TablemapSchema
