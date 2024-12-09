4f16ec964f9d81f4387d79118c9af355ab421f10
Bastien Seree <bastien.seree@wappizy.com>
2024-12-09 12:48:51 +0100

    EDF: [#68] add name field to  eventCategory schema

--- backend/web/server/plugins/entreprisedufutur/schemas/EventCategorySchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/EventCategorySchema.js
@@ -4,6 +4,10 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const EventCategorySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, `Le nom de la catégorie d'événement est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
