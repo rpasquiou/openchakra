806b1ced8d99139baf49bf3feac297eeab9584ad
Bastien Seree <bastien.seree@wappizy.com>
2024-12-09 12:48:04 +0100

    EDF: [#68] add eventCategory schema

---
+++ backend/web/server/plugins/entreprisedufutur/schemas/EventCategorySchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const {schemaOptions} = require('../../../utils/schemas')
+
+const Schema = mongoose.Schema
+
+const EventCategorySchema = new Schema({
+}, {...schemaOptions})
+
+/* eslint-disable prefer-arrow-callback */
+/* eslint-enable prefer-arrow-callback */
+
+module.exports = EventCategorySchema
