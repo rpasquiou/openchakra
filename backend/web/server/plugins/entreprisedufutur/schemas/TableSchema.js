953524423c2ca8b6ab948cebc16f220fcd65de73
Bastien Seree <bastien.seree@wappizy.com>
2024-12-09 10:42:37 +0100

    EDF: [#76] add tablemap field to table schema

--- backend/web/server/plugins/entreprisedufutur/schemas/TableSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/TableSchema.js
@@ -4,6 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const TableSchema = new Schema({
+  tablemap: {
+    type: Schema.Types.ObjectId,
+    ref: 'tablemap',
+    required: [true, `Le plan de table de la table est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
