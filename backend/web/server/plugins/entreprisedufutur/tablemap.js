11f91338a499cbda6fd2c3a90a0bfb6281d537e9
Bastien Seree <bastien.seree@wappizy.com>
2024-12-10 16:30:47 +0100

    EDF: tablemap computing draft

---
+++ backend/web/server/plugins/entreprisedufutur/tablemap.js
@@ -0,0 +1,19 @@
+const generateTables = async (eventId) => {
+
+}
+
+const computeTableMap = async (eventId) => {
+
+  //récupérer les tables
+  
+  //récupérer les voeux
+  //si voeu présent à 1 seule table -> affecté à la table
+  
+  //on commence par ambdassaeur
+  //puis parrains
+  //puis contributeurs
+    //on fait -> pour chaque voeu -> on l'attribue au partner (avec des places restantes) avec le moins de voeux restants
+    //on vérifie -> si partners avec écart de voeux attribué > 2 -> ? (trouver moyen d'égaliser)
+  
+  //on remplit les places libres avec les gens qui restent (contraintes sur le remplissage ?)
+}
