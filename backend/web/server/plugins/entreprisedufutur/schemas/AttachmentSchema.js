5670d865cec24829675faa304002f50c66d203cc
Bastien Seree <bastien.seree@wappizy.com>
2024-12-10 11:56:40 +0100

    EDF: [#89] add name field to attachment schema

--- backend/web/server/plugins/entreprisedufutur/schemas/AttachmentSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/AttachmentSchema.js
@@ -4,7 +4,10 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const AttachmentSchema = new Schema({
-  
+  name: {
+    type: String,
+    required: [true, `Le nom de la pièce jointe est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
