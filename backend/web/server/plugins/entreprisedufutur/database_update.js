7e7eb47988c3fff23da15b54709571e56252d421
Bastien Seree <bastien.seree@wappizy.com>
2024-12-12 16:12:36 +0100

    EDF: comment database updates not in use

--- backend/web/server/plugins/entreprisedufutur/database_update.js
+++ backend/web/server/plugins/entreprisedufutur/database_update.js
@@ -14,109 +14,109 @@ const error = (...params) => {
     return console.error(`DB Update`, ...params)
 }
 
-const normalizeRoles = async () => {
-  log(`Normalizing roles`)
+// const normalizeRoles = async () => {
+//   log(`Normalizing roles`)
 
-  const MAPPING={
-    MEMBER: ROLE_MEMBER,
-    ADMIN: ROLE_ADMIN,
-    PARTNER: ROLE_PARTNER
-  }
-  return Promise.all(Object.entries(MAPPING).map(([oldRole, newRole]) => 
-    User.updateMany({role:oldRole},{role: newRole})
-  ))
-}
+//   const MAPPING={
+//     MEMBER: ROLE_MEMBER,
+//     ADMIN: ROLE_ADMIN,
+//     PARTNER: ROLE_PARTNER
+//   }
+//   return Promise.all(Object.entries(MAPPING).map(([oldRole, newRole]) => 
+//     User.updateMany({role:oldRole},{role: newRole})
+//   ))
+// }
 
-const updateCompanyAdmin = async () => {
-  log(`Updating admin attribute`)
+// const updateCompanyAdmin = async () => {
+//   log(`Updating admin attribute`)
 
-  const updateAdmin = async (company) => {
-    const admin=company.toObject().admin
-    Company.collection.updateOne({_id: company._id}, {$set: {administrators: [admin]}, $unset: {admin: 1}})
-  }
-  return Company.find({admin:{$ne: undefined}})
-    .then(companies => Promise.all(companies.map(c => updateAdmin(c))))
-}
+//   const updateAdmin = async (company) => {
+//     const admin=company.toObject().admin
+//     Company.collection.updateOne({_id: company._id}, {$set: {administrators: [admin]}, $unset: {admin: 1}})
+//   }
+//   return Company.find({admin:{$ne: undefined}})
+//     .then(companies => Promise.all(companies.map(c => updateAdmin(c))))
+// }
 
-const normalizeJobs = async () => {
-  log(`Normalizing jobs`)
+// const normalizeJobs = async () => {
+//   log(`Normalizing jobs`)
 
-  const MAPPING={
-    COMMERCIIAL_MANAGER: JOB_COMMERCIAL_MANAGER,
-    GENERAL_MANAGER: JOB_GENERAL_MANAGER
-  }
-  return Promise.all(Object.entries(MAPPING).map(([oldJob, newJob]) => 
-    User.updateMany({job:oldJob},{job: newJob})
-  ))
-}
+//   const MAPPING={
+//     COMMERCIIAL_MANAGER: JOB_COMMERCIAL_MANAGER,
+//     GENERAL_MANAGER: JOB_GENERAL_MANAGER
+//   }
+//   return Promise.all(Object.entries(MAPPING).map(([oldJob, newJob]) => 
+//     User.updateMany({job:oldJob},{job: newJob})
+//   ))
+// }
 
-const normalizeCompanySize = async () => {
-  log(`Normalizing company sizes`)
+// const normalizeCompanySize = async () => {
+//   log(`Normalizing company sizes`)
 
-  const MAPPING={
-    COMPANY_SIZE_11_250: COMPANY_SIZE_11_50,
-    COMPANY_SIZE_5001_PLUS: COMPANY_SIZE_1001_PLUS
-  }
+//   const MAPPING={
+//     COMPANY_SIZE_11_250: COMPANY_SIZE_11_50,
+//     COMPANY_SIZE_5001_PLUS: COMPANY_SIZE_1001_PLUS
+//   }
 
-  await Company.updateMany({size: null}, {size:COMPANY_SIZE_11_50})
+//   await Company.updateMany({size: null}, {size:COMPANY_SIZE_11_50})
 
-  return Promise.all(Object.entries(MAPPING).map(([oldSize, newSize]) => 
-    Company.updateMany({size:oldSize},{size: newSize})
-  ))
-}
+//   return Promise.all(Object.entries(MAPPING).map(([oldSize, newSize]) => 
+//     Company.updateMany({size:oldSize},{size: newSize})
+//   ))
+// }
 
-const addExpertiseSet = async () => {
+// const addExpertiseSet = async () => {
 
-  const filter = {expertise_set: {$exists: false}}
-  const blankExpertiseSet = {expertises: [], categories: []}
+//   const filter = {expertise_set: {$exists: false}}
+//   const blankExpertiseSet = {expertises: [], categories: []}
 
-  log(`Adding expertiseSet to users`)
-  const users = await User.find(filter)
+//   log(`Adding expertiseSet to users`)
+//   const users = await User.find(filter)
 
-  users.forEach(async (u) => {
-    expSet = await ExpertiseSet.create(blankExpertiseSet)
-    await User.findByIdAndUpdate({_id: u._id},{expertise_set: expSet._id})
-  })
+//   users.forEach(async (u) => {
+//     expSet = await ExpertiseSet.create(blankExpertiseSet)
+//     await User.findByIdAndUpdate({_id: u._id},{expertise_set: expSet._id})
+//   })
 
-  log(`Adding expertiseSet to groups`)
-  const groups = await Group.find(filter)
+//   log(`Adding expertiseSet to groups`)
+//   const groups = await Group.find(filter)
 
-  groups.forEach(async (g) => {
-    expSet = await ExpertiseSet.create(blankExpertiseSet)
-    await Group.findByIdAndUpdate({_id: g._id},{expertise_set: expSet._id})
-  })
+//   groups.forEach(async (g) => {
+//     expSet = await ExpertiseSet.create(blankExpertiseSet)
+//     await Group.findByIdAndUpdate({_id: g._id},{expertise_set: expSet._id})
+//   })
 
-  log(`Adding expertiseSet to companies`)
-  const companies = await Company.find(filter)
+//   log(`Adding expertiseSet to companies`)
+//   const companies = await Company.find(filter)
 
-  companies.forEach(async (c) => {
-    expSet = await ExpertiseSet.create(blankExpertiseSet)
-    await Company.findByIdAndUpdate({_id: c._id},{expertise_set: expSet._id})
-  })
+//   companies.forEach(async (c) => {
+//     expSet = await ExpertiseSet.create(blankExpertiseSet)
+//     await Company.findByIdAndUpdate({_id: c._id},{expertise_set: expSet._id})
+//   })
 
-  log(`Adding expertiseSet to contents`)
-  const contents = await Content.find(filter)
+//   log(`Adding expertiseSet to contents`)
+//   const contents = await Content.find(filter)
 
-  contents.forEach(async (c) => {
-    expSet = await ExpertiseSet.create(blankExpertiseSet)
-    await Content.findByIdAndUpdate({_id: c._id},{expertise_set: expSet._id})
-  })
+//   contents.forEach(async (c) => {
+//     expSet = await ExpertiseSet.create(blankExpertiseSet)
+//     await Content.findByIdAndUpdate({_id: c._id},{expertise_set: expSet._id})
+//   })
 
-  log(`Adding expertiseSet to events`)
-  const events = await Event.find(filter)
+//   log(`Adding expertiseSet to events`)
+//   const events = await Event.find(filter)
 
-  events.forEach(async (e) => {
-    expSet = await ExpertiseSet.create(blankExpertiseSet)
-    await Event.findByIdAndUpdate({_id: e._id},{expertise_set: expSet._id})
-  })
+//   events.forEach(async (e) => {
+//     expSet = await ExpertiseSet.create(blankExpertiseSet)
+//     await Event.findByIdAndUpdate({_id: e._id},{expertise_set: expSet._id})
+//   })
 
-}
+// }
 
-const addSector = async () => {
-  log('Add sector to companies without one')
+// const addSector = async () => {
+//   log('Add sector to companies without one')
 
-  await Company.updateMany({sector: {$exists: false}}, {sector: SECTOR_AERONAUTICS})// if not exists
-}
+//   await Company.updateMany({sector: {$exists: false}}, {sector: SECTOR_AERONAUTICS})// if not exists
+// }
 
 const databaseUpdate = async () => {
   console.log('************ UPDATING DATABASE')
