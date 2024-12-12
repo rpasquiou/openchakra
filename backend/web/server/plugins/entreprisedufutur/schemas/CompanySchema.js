b93c1eb55f64c83861f66f4a276d3a3ea2987572
Bastien Seree <bastien.seree@wappizy.com>
2024-12-12 16:12:12 +0100

    EDF: [#106] adapt consts for company size to match new specs

--- backend/web/server/plugins/entreprisedufutur/schemas/CompanySchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/CompanySchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { schemaOptions } = require('../../../utils/schemas')
 const siret = require('siret')
 const { DUMMY_REF } = require('../../../utils/database')
-const { SECTOR, COMPANY_SIZE, LOOKING_FOR_MISSION, STATUTS, STATUT_FOUNDER, STATUT_SPONSOR } = require('../consts')
+const { SECTOR, COMPANY_SIZES, LOOKING_FOR_MISSION, STATUTS, STATUT_FOUNDER, STATUT_SPONSOR } = require('../consts')
 const { isPhoneOk, isEmailOk } = require('../../../../utils/sms')
 const AddressSchema = require('../../../models/AddressSchema')
 
@@ -19,7 +19,7 @@ const CompanySchema = new Schema(
     },
     size: {
       type: String,
-      enum: Object.keys(COMPANY_SIZE),
+      enum: Object.keys(COMPANY_SIZES),
       required: true,
     },
     administrators: {
