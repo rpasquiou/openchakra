cc86f8652c3903705f02fa800741ff84b0e921f7
Bastien Seree <bastien.seree@wappizy.com>
2024-12-17 15:27:13 +0100

    EDF: [#119] add event_ticket field to order schema

--- backend/web/server/plugins/entreprisedufutur/schemas/OrderSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/OrderSchema.js
@@ -4,7 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const OrderSchema = new Schema({
-
+  event_ticket: {
+    type: Schema.Types.ObjectId,
+    ref: 'eventTicket',
+    required: [true, `Le type de ticket est obligatoire`]
+  },
 }, schemaOptions)
 
 /* eslint-disable prefer-arrow-callback */
