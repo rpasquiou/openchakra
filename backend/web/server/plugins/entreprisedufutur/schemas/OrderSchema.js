c9d3c694da8cdf6d0ee8259ded1bf6d5682eb2b1
Bastien Seree <bastien.seree@wappizy.com>
2024-12-17 15:33:07 +0100

    EDF: [#127] add status field to order schema

--- backend/web/server/plugins/entreprisedufutur/schemas/OrderSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/OrderSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
+const { ORDER_STATUSES } = require('../consts')
 
 const Schema = mongoose.Schema
 
@@ -9,6 +10,11 @@ const OrderSchema = new Schema({
     ref: 'eventTicket',
     required: [true, `Le type de ticket est obligatoire`]
   },
+  status: {
+    type: String,
+    enum:Object.keys(ORDER_STATUSES),
+    required: [true, `Le statut de la commande est obligatoire`]
+  }
 }, schemaOptions)
 
 /* eslint-disable prefer-arrow-callback */
