0919e53c241c77291c741e334e41e76fa2214a6d
Bastien Seree <bastien.seree@wappizy.com>
2024-12-17 15:39:05 +0100

    EDF: [#125] add order field to  orderTicket schema

--- backend/web/server/plugins/entreprisedufutur/schemas/OrderTicketSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/OrderTicketSchema.js
@@ -4,6 +4,11 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const OrderTicketSchema = new Schema({
+  order: {
+    type: Schema.Types.ObjectId,
+    ref: 'order',
+    required: [true, `La commande du ticket est obligatoire`]
+  },
   name: {
     type: String,
     required: false
