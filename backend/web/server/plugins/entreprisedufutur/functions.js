1d72d86a3df0aa87b1a574337610819c8769e046
Bastien Seree <bastien.seree@wappizy.com>
2024-12-19 10:22:27 +0100

    EDF: [#139] add buy_quantity field to eventTicket schema

--- backend/web/server/plugins/entreprisedufutur/functions.js
+++ backend/web/server/plugins/entreprisedufutur/functions.js
@@ -481,6 +481,7 @@ declareEnumField({model: 'eventTicket', field: 'targeted_roles', multiple: true,
 declareEnumField({model: 'eventTicket', field: 'visibility', enumValues: EVENT_VISIBILITY})
 declareVirtualField({model: 'eventTicket', field: 'quantity_registered', instance: 'Number'})
 declareVirtualField({model:'eventTicket', field: 'remaining_tickets', instance: 'Number'})
+declareVirtualField({model: 'eventTicket', field: 'buy_quantity', multiple: true, instance: 'Array'})
 
 //UserTicket declarations
 declareEnumField({model: 'userTicket', field: 'status', enumValues: USERTICKET_STATUSES})
