afa1b8629efaab89c61c41d90c5324b5df3f6f5b
Bastien Seree <bastien.seree@wappizy.com>
2024-12-24 10:12:07 +0100

    EDF: [#162] add getter for events field to user schema

--- backend/web/server/plugins/entreprisedufutur/user.js
+++ backend/web/server/plugins/entreprisedufutur/user.js
@@ -1,5 +1,7 @@
 const lodash = require('lodash')
 const { loadFromDb } = require('../../utils/database')
+const UserTicket = require('../../models/UserTicket')
+const EventTicket = require('../../models/EventTicket')
 
 const getLooking = async function () {
   const looking = await loadFromDb({model: 'user', fields: ['looking_for_opportunities']})
@@ -10,4 +12,14 @@ const getLooking = async function () {
   return ids
 }
 
-module.exports = { getLooking }
+const getEvents = async function (userId, params, data) {
+  const userTickets = await UserTicket.find({user: data._id})
+  const eventTicketsIds = userTickets.map((ticket)=> {return ticket.event_ticket})
+  const eventTickets = await EventTicket.find({_id: {$in: eventTicketsIds}})
+  return eventTickets.map(ticket => ticket.event)
+}
+
+module.exports = { 
+  getLooking,
+  getEvents,
+ }
