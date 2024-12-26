fa56605a8188d8a7864bf6cbe01dac1c5c0ec0c7
Bastien Seree <bastien.seree@wappizy.com>
2024-12-26 09:56:46 +0100

    EDF: [#171] only take registered and payed tickets for events virtual in user schema

--- backend/web/server/plugins/entreprisedufutur/user.js
+++ backend/web/server/plugins/entreprisedufutur/user.js
@@ -2,6 +2,7 @@ const lodash = require('lodash')
 const { loadFromDb } = require('../../utils/database')
 const UserTicket = require('../../models/UserTicket')
 const EventTicket = require('../../models/EventTicket')
+const { USERTICKET_STATUS_PAYED, USERTICKET_STATUS_PENDING_PAYMENT, USERTICKET_STATUS_REGISTERED } = require('./consts')
 
 const getLooking = async function () {
   const looking = await loadFromDb({model: 'user', fields: ['looking_for_opportunities']})
@@ -13,7 +14,7 @@ const getLooking = async function () {
 }
 
 const getEvents = async function (userId, params, data) {
-  const userTickets = await UserTicket.find({user: data._id})
+  const userTickets = await UserTicket.find({user: data._id,status: {$in: [USERTICKET_STATUS_PAYED, USERTICKET_STATUS_PENDING_PAYMENT,USERTICKET_STATUS_REGISTERED]}})
   const eventTicketsIds = userTickets.map((ticket)=> {return ticket.event_ticket})
   const eventTickets = await EventTicket.find({_id: {$in: eventTicketsIds}})
   return eventTickets.map(ticket => ticket.event)
