efd16bbe23508c48ab8ea3a712b3cb809d66860d
Bastien Seree <bastien.seree@wappizy.com>
2024-12-26 11:17:46 +0100

    EDF: [#173] take requested fields into account in registered users getter for event schema

--- backend/web/server/plugins/entreprisedufutur/event.js
+++ backend/web/server/plugins/entreprisedufutur/event.js
@@ -1,11 +1,20 @@
 const UserTicket = require('../../models/UserTicket')
 const EventTicket = require('../../models/EventTicket')
+const User = require('../../models/User')
 
-const getRegistered = async function (userId, params, data) {
+const getRegistered = async function (userId, params, data,fields) {
   const eventTickets = await EventTicket.find({event: data._id})
   const eventTicketIds = eventTickets.map(ticket => ticket._id)
   const userTickets = await UserTicket.find({event_ticket: {$in: eventTicketIds}})
-  return userTickets.map(ticket=> ticket.user)
+  const users = await loadFromDb({
+    model: 'event',
+    user: userId,
+    fields,
+    params: {...params,
+              'filter._id':{$in: userTickets.map(u => u._id)},
+            }
+  })
+  return users.map(u=> new User(u))
 }
 
 const getRegisteredNumber = async function (userId, params, data) {
