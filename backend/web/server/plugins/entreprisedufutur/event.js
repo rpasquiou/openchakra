3345e7b3b175a0aed0a17b01914b96589b4e49a7
Bastien Seree <bastien.seree@wappizy.com>
2024-12-26 15:40:04 +0100

    EDF: typo in registered_users getter

--- backend/web/server/plugins/entreprisedufutur/event.js
+++ backend/web/server/plugins/entreprisedufutur/event.js
@@ -7,15 +7,14 @@ const { loadFromDb } = require('../../utils/database')
 const getRegistered = async function (userId, params, data,fields) {
   const eventTickets = await EventTicket.find({event: data._id})
   const eventTicketIds = eventTickets.map(ticket => ticket._id)
-  const userTickets = await UserTicket.find({event_ticket: {$in: eventTicketIds}})
+  const userTickets = await UserTicket.find({event_ticket: {$in: eventTicketIds}, status: {$in: [USERTICKET_STATUS_PAYED, USERTICKET_STATUS_PENDING_PAYMENT,USERTICKET_STATUS_REGISTERED]}})
+  console.log('fields',fields);
+  
   const users = await loadFromDb({
-    model: 'event',
+    model: 'user',
     user: userId,
     fields,
-    params: {...params,
-              'filter._id':{$in: userTickets.map(u => u._id)},
-              'filter.status': {$in: [USERTICKET_STATUS_PAYED, USERTICKET_STATUS_PENDING_PAYMENT,USERTICKET_STATUS_REGISTERED]}
-            }
+    params: {...params,'filter._id':{$in: userTickets.map(u => u._id)}}
   })
   return users.map(u=> new User(u))
 }
