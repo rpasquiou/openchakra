956b6b7d3c8f948369dc0b5fcd1929944bcea6d9
Sébastien Auvray <sebastien.auvray@wappizy.com>
2025-01-09 01:14:13 +0100

    Revert "Back: getBlockResources uses Block.progresses"
    This reverts commit e6b7ffe25e764da551a2d738d201cfd35e76eab7.

--- backend/web/server/plugins/aftral-lms/resources.js
+++ backend/web/server/plugins/aftral-lms/resources.js
@@ -26,16 +26,11 @@ const getBlockResourcesNew = async ({ blockId, userId, includeUnavailable, inclu
     role = (await User.findById(userId))?.role;
   }
 
-  let query = Block.find({ parent: blockId, masked: { $ne: true } })
+  const blocks = await Block.find({ parent: blockId, masked: { $ne: true } })
     .select({ _id: 1, type: 1, order: 1, optional: 1 })
-    .lean({ virtuals: false })
-    .sort({ order: 1 })
+    .lean({virtuals: false})
+    .sort({ order: 1 });
 
-  // Load unavailable progress
-  if (role == ROLE_APPRENANT && !includeUnavailable) {
-    query=query.populate({ path: 'progresses', match: { achievement_status: BLOCK_STATUS_UNAVAILABLE }, select: { _id: 1 } })
-  }
-  const blocks = await query
   let res = [];
 
   // Process each block, and ensure correct ordering of results
@@ -46,7 +41,7 @@ const getBlockResourcesNew = async ({ blockId, userId, includeUnavailable, inclu
     }
     // Don't include unavailable: skip
     if (role == ROLE_APPRENANT && !includeUnavailable) {
-      const unavailable =  b.progresses.length>0
+      const unavailable = await Progress.exists({block: b._id,user: userId,achievement_status: BLOCK_STATUS_UNAVAILABLE})
       if (unavailable) {
         continue
       }
