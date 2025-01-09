b34f00eb2ea84fb68c99ac3254be921bab662190
Bastien Seree <bastien.seree@wappizy.com>
2025-01-09 09:54:52 +0100

    EDF: [#205] add name field to target schema

--- backend/web/server/plugins/entreprisedufutur/schemas/TargetSchema.js
+++ backend/web/server/plugins/entreprisedufutur/schemas/TargetSchema.js
@@ -5,7 +5,10 @@ const {schemaOptions} = require('../../../utils/schemas')
 const Schema = mongoose.Schema
 
 const TargetSchema = new Schema({
-  
+  name: {
+    type: String,
+    required: [true, `Le nom de la cible est obligatoire`]
+  },
 }, schemaOptions  )
 
 module.exports = TargetSchema
