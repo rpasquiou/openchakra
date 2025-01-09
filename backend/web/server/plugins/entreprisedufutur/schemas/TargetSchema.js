1520f0072f16cf6e99dd02c120b8329239bf2cd0
Bastien Seree <bastien.seree@wappizy.com>
2025-01-09 09:54:00 +0100

    EDF: [#205] add target schema

---
+++ backend/web/server/plugins/entreprisedufutur/schemas/TargetSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const {schemaOptions} = require('../../../utils/schemas')
+
+
+const Schema = mongoose.Schema
+
+const TargetSchema = new Schema({
+  
+}, schemaOptions  )
+
+module.exports = TargetSchema
