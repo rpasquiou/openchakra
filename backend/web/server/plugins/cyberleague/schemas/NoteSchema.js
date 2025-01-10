1a6ff533052b3d8feca3ec9bb23edace706ef982
Bastien Seree <bastien.seree@wappizy.com>
2025-01-10 10:53:43 +0100

    Cyberleague: [#374] add type field to note schema

--- backend/web/server/plugins/cyberleague/schemas/NoteSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/NoteSchema.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
+const { NOTE_TYPES } = require('../consts')
 const Schema = mongoose.Schema
 
 let NoteSchema = new Schema({
-
+  type: {
+    type: String,
+    enum: Object.keys(NOTE_TYPES),
+    required: [true, `Le type de la note est obligatoire`]
+  },
 }, {...schemaOptions})
 
 /* eslint-disable prefer-arrow-callback */
