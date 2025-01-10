ef89096b1e2d674e899464ba07325cd1f0e91de5
Bastien Seree <bastien.seree@wappizy.com>
2025-01-10 11:39:48 +0100

    Cyberleague: [#374] add validate for livefeed field in adminDashboard schema

--- backend/web/server/plugins/cyberleague/schemas/AdminDashboardSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/AdminDashboardSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
 const { DUMMY_REF } = require('../../../utils/database')
+const { LIVEFEED_MAX_LENGTH } = require('../consts')
 
 const Schema = mongoose.Schema
 
@@ -17,6 +18,7 @@ const AdminDashboardSchema = new Schema({
       required: true
     }],
     default: [],
+    validate: [(v) => {v?.length < LIVEFEED_MAX_LENGTH},`Le livefeed doit être au maximum de taille ${LIVEFEED_MAX_LENGTH}`],
     required: false
   },
 }, {...schemaOptions})
