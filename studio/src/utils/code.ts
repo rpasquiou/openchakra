cfb331e2f8b3ef22377c021a3e0c950fbc9b4ef8
Sébastien Auvray <sebastien.auvray@wappizy.com>
2025-01-10 10:24:57 +0100

    Front: remove extra log

--- studio/src/utils/code.ts
+++ studio/src/utils/code.ts
@@ -288,7 +288,6 @@ const buildBlock = ({
           if (((childComponent.props.dataSource && tp?.type) || childComponent.props.model) && childComponent.props?.attribute) {
             const att=models[tp?.type || childComponent.props.model].attributes[childComponent.props?.attribute]
             if (att?.enumValues && (childComponent.type!='RadioGroup' || lodash.isEmpty(childComponent.children))) {
-              console.log(att.enumValues)
               propsContent += ` enum='${encode(JSON.stringify(att.enumValues))}'`
             }
             if (att?.suggestions) {
