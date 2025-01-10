2c5904740220f223cfae1068ab820707c9ecb4cb
Bastien Seree <bastien.seree@wappizy.com>
2025-01-10 11:17:10 +0100

    Cyberleague: [#374] add requirement and validators for global_rate and nutriscore fields in note schema

--- backend/web/server/plugins/cyberleague/schemas/NoteSchema.js
+++ backend/web/server/plugins/cyberleague/schemas/NoteSchema.js
@@ -22,9 +22,13 @@ let NoteSchema = new Schema({
   nutriscore: {
     type: String,
     enum: Object.keys(NUTRISCORE),
+    validate: [function(value) {!(value && this.global_rate)}, `Une note ne peut pas avoir un score et un nutriscore`],
+    required: [function () {!this.global_rate}, `Une note doit avoir soit un score, soit un nutriscore`],
   },
   global_rate: {
     type: Number,
+    validate: [function(value) {!(value && this.nutriscore)}, `Une note ne peut pas avoir un score et un nutriscore`],
+    required: [function () {!this.nutriscore}, `Une note doit avoir soit un score, soit un nutriscore`],
   },
 }, {...schemaOptions})
 
