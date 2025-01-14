8eb2f4f60cedc3431e3b93352934f92423b3b282
Sébastien Auvray <sebastien.auvray@wappizy.com>
2025-01-14 17:58:09 +0100

    Aftral: script lockSession now requires trainee

--- backend/web/scripts/aftral-lms/lockSession.js
+++ backend/web/scripts/aftral-lms/lockSession.js
@@ -5,25 +5,31 @@ const { lockSession } = require('../../server/plugins/aftral-lms/block')
 const { getDatabaseUri } = require('../../config/config')
 const Progress = require('../../server/models/Progress')
 const { getBlockChildren } = require('../../server/plugins/aftral-lms/resources')
+const User = require('../../server/models/User')
 
-const lockTheSession = async sessionId => {
+const lockTheSession = async (sessionId, traineeId) => {
+  await mongoose.connect(getDatabaseUri(), MONGOOSE_OPTIONS)
+  const trainee=await User.findById(traineeId)
+  if (!trainee) {
+    throw new Error(`Trainee ${traineeId} not found`)
+  }
   await mongoose.connect(getDatabaseUri(), MONGOOSE_OPTIONS)
   
   const children = await getBlockChildren({ blockId: sessionId })
-  await Progress.remove({block: {$in: [sessionId, ...children]}})
-  await lockSession(sessionId)
+  await Progress.remove({user: trainee, block: {$in: [sessionId, ...children]}})
+  await lockSession(sessionId, trainee)
   const childrenCount=children.length
   const progressCount=await Progress.countDocuments()
   console.log(childrenCount, progressCount)
   }
 
-const sessionId=process.argv[2]
-if (!sessionId) {
-  console.error(`Usage: ${process.argv[0]} ${process.argv[1]} sessionId`)
+const [sessionId, traineeId]=process.argv.slice(2, 4)
+if (!sessionId || !traineeId) {
+  console.error(`Usage: ${process.argv[0]} ${process.argv[1]} sessionId traineeId`)
   process.exit(1)
 }
 
-lockTheSession(sessionId)
+lockTheSession(sessionId, traineeId)
   .then(console.log)
   .catch(console.error)
   .finally(() => process.exit(0))
